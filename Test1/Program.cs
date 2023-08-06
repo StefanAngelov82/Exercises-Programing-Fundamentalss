9b09e5028441e4626208413e97ac491bac24d450
Stefan Angelov <ashmodean_@abv.bg>
2023-08-06 16:41:22 +0300

    first test

---
+++ Test1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < number; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine(string.Join(", ", list));
+
+        }
+    }
+}
