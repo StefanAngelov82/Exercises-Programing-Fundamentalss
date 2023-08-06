b4cc907b0dd26fd15656291c64589253f55d2db7
Stefan Angelov <ashmodean_@abv.bg>
2023-08-06 21:27:31 +0300

    Upload firs task

---
+++ 01.Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sum_Adjacent_Equal_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<decimal> numbers1 = Console.ReadLine()
+                .Split(' ')
+                .Select(decimal.Parse)
+                .ToList();
+
+            for (int i = 0; i < numbers1.Count - 1; i++)
+            {
+                if (numbers1[i] == numbers1[i + 1])
+                {
+                    numbers1[i] = numbers1[i] + numbers1[i + 1];
+                    numbers1.RemoveAt(i + 1);
+                    i = - 1;
+                }
+            }
+
+            foreach (var digit in numbers1)
+            {
+                Console.Write(digit + " ");
+            }
+        }
+    }
+}
