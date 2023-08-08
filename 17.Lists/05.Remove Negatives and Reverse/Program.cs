3857880846d7eb4d746557e211a31c1ed7a5c50b
Stefan Angelov <ashmodean_@abv.bg>
2023-08-08 20:34:57 +0300

    update task 5

---
+++ 17.Lists/05.Remove Negatives and Reverse/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Remove_Negatives_and_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<int> list = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            int counter = list.Count;
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] < 0)
+                {
+                    list.RemoveAt(i);
+                    counter--;
+                    i--;
+                }
+            }
+
+            if (counter == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                list.Reverse();
+                Console.WriteLine(string.Join(' ', list));
+            }
+        }
+    }
+}
