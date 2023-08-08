0d89845ebce5f0087451b29aede388807ef71b7c
Stefan Angelov <ashmodean_@abv.bg>
2023-08-08 20:07:51 +0300

    task update

---
+++ 17.Lists/04.List of Products/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List_of_Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> products = new List<string>();
+            int p = n;
+
+            while (p > 0)
+            {
+                products.Add(Console.ReadLine());
+                p--;
+            }
+
+            products.Sort();
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+            
+        }
+    }
+}
