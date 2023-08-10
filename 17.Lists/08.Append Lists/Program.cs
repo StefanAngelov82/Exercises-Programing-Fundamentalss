95a7db544bef5ae43bfcbaffefd3843d4a02e90d
Stefan Angelov <ashmodean_@abv.bg>
2023-08-10 12:30:17 +0300

    upload task 7

---
+++ 17.Lists/08.Append Lists/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Append_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine().Split("|").Reverse().ToList();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                list[i] = list[i].Trim();
+            }         
+            Console.WriteLine(string.Join(" ", list));
+        }
+    }
+}
