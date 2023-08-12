c4b4968da439b28c18546cb982112670474df523
Stefan Angelov <ashmodean_@abv.bg>
2023-08-12 22:56:52 +0300

    update task 18

---
+++ 17.Lists/18.Remove Elements at Odd Positions/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Remove_Elements_at_Odd_Positions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> wards = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+            List<string> newWards = new List<string>();
+
+            for (int i = 0; i < wards.Count; i++)
+            {
+                if ((i % 2) != 0)
+                {
+                    newWards.Add(wards[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join("", newWards));
+        }
+    }
+}
