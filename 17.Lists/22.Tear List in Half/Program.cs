01d441e607cb5d25968b993fc91d31cf6fb446b3
Stefan Angelov <ashmodean_@abv.bg>
2023-08-13 23:08:19 +0300

    upload task 22

---
+++ 17.Lists/22.Tear List in Half/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Tear_List_in_Half
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] initialNumbers = Console.ReadLine().Split(' ');
+
+            int middle = initialNumbers.Length / 2;
+
+            string[] partOne = initialNumbers[0..middle];
+            string[] partTwo = initialNumbers[middle..];
+
+            List<string> result = new List<string>();
+
+            for (int i = 0; i < partOne.Length; i++)
+            {
+                string firstElement = partTwo[i].First().ToString();
+                string secondElement = partTwo[i].Last().ToString();
+
+                result.Add(firstElement);
+                result.Add(partOne[i]);
+                result.Add(secondElement);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+            
+        }
+    }
+}
