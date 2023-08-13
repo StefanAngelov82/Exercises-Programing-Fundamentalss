569c71c3024e731327e6fde849360cdcd68ee5cd
Stefan Angelov <ashmodean_@abv.bg>
2023-08-13 22:31:46 +0300

    upload task 21

---
+++ 17.Lists/21.Flip List Sides/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Flip_List_Sides
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            for (int i = 1; i < (list.Count / 2); i++)
+            {
+                int temp = list[i];
+                list[i] = list[list.Count - 1 - i];
+                list[list.Count - 1 - i] = temp;
+            }
+
+            Console.WriteLine(string.Join(' ', list));
+        }
+    }
+}
