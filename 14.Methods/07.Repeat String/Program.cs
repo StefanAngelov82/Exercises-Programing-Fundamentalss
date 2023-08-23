764ed0aeece899b48b0406795100f567c5886a17
Stefan Angelov <ashmodean_@abv.bg>
2023-08-23 22:56:36 +0300

    upload task 7

---
+++ 14.Methods/07.Repeat String/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int repitition = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RepeatString(input, repitition));
+        }
+
+        static string RepeatString(string input, int repitition)
+        {
+            string result = String.Empty;
+
+            for (int i = 0; i < repitition; i++)
+            {
+                result += input;
+            }
+
+            return result;
+        }
+
+    }
+}
