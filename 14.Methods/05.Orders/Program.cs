184999376f5cc9b16a2128aac5a5de7f0fc9bf4a
Stefan Angelov <ashmodean_@abv.bg>
2023-08-23 22:54:10 +0300

    upload task 5

---
+++ 14.Methods/05.Orders/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            double price = quantity * Productprice(product);
+
+            Console.WriteLine($"{price:f2}");
+        }
+
+        static double Productprice(string product)
+        {
+            double productPrice = 0;
+
+            switch (product)
+            {
+                case "coffee":
+                    productPrice = 1.50;
+                    break;
+                case "water":
+                    productPrice = 1.00;
+                    break;
+                case "coke":
+                    productPrice = 1.40;
+                    break;
+                case "snacks":
+                    productPrice = 2.00;
+                    break;
+            }
+
+            return productPrice;
+        }
+    }
+}
