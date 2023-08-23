4f2d4c45f5893c29e7cee9cdd99d3e2eef4c6fb7
Stefan Angelov <ashmodean_@abv.bg>
2023-08-23 22:55:44 +0300

    upload task 6

---
+++ 14.Methods/06.Calculate Rectangle Area/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Calculate_Rectangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = double.Parse(Console.ReadLine());
+            var y = double.Parse(Console.ReadLine());
+
+            double area = Area(x,y);
+
+            Console.WriteLine(area);
+        }
+
+        static double Area(double x, double y)
+        {
+            double area =  x * y;
+
+            return area;
+        }
+        static int Area(int x, int y)
+        {
+            int area = x * y;
+
+            return area;
+        }
+    }
+}
