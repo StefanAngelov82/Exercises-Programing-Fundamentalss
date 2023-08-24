381473eacdbc587f1469805f64231037fea315b3
Stefan Angelov <ashmodean_@abv.bg>
2023-08-24 17:50:20 +0300

    upload task 32

---
+++ 17.Lists/32.Shoot List Elements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Shoot_List_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
