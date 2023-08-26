8cc4c2fe4fa76bb9292e4d9e5ae20244978612a0
Stefan Angelov <ashmodean_@abv.bg>
2023-08-26 13:58:11 +0300

    upload task 37

---
+++ 17.Lists/37.Decode Radio Frequencies/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Decode_Radio_Frequencies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] deliminers = {' ','.'};
+            string[] numbers = Console.ReadLine()
+                .Split(deliminers)
+                .ToArray();
+
+            List<char> leftList = new List<char>();
+            List<char> rightList = new List<char>();
+            List<char> result = new List<char>();
+
+            char left = ' ', right = ' ';
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    left = (char)int.Parse(numbers[i]);
+                    leftList.Add(left);
+                }
+                else
+                {
+                    right = (char)int.Parse(numbers[i]);
+                    rightList.Add(right);
+                }               
+            }
+
+            rightList.Reverse();
+
+            result.InsertRange(0,rightList);
+            result.InsertRange(0,leftList);
+
+            Console.WriteLine(string.Join("", result));
+        }
+    }
+}
