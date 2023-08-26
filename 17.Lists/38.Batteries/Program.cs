ea3822ba084e6bad11c3d2a4906c0084749ff26a
Stefan Angelov <ashmodean_@abv.bg>
2023-08-26 17:55:49 +0300

    upload task 38

---
+++ 17.Lists/38.Batteries/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Batteries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           List<double> Battarylife = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToList();
+            List<double> LoadPerHour = Console.ReadLine()
+                .Split(" ")
+                .Select(double.Parse)
+                .ToList();
+            int testhours = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < Battarylife.Count; i++)
+            {
+                double LoadForTest = LoadPerHour[i] * testhours;
+
+                if (Battarylife[i] >= LoadForTest )
+                {
+                    double leftLife = Battarylife[i] - LoadForTest;
+
+                    Console.WriteLine($"Battery {i + 1}: {leftLife:F2} mAh ({(leftLife / Battarylife[i]) * 100:F2})%");
+                }
+                else
+                {
+                    double hoursSustainLoad = (Math.Ceiling(Battarylife[i] / LoadPerHour[i]));
+
+                    Console.WriteLine($"Battery {i + 1}: dead (lasted {hoursSustainLoad} hours)");
+                }
+            }
+        }
+    }
+}
