4f5a622f531397bd446d6aea8c271c82bcc02713
Stefan Angelov <ashmodean_@abv.bg>
2023-08-27 17:00:27 +0300

    upload task 40

---
+++ 17.Lists/40.Increasing Crisis/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Increasing_Crisis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            List<int> numbers= Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();           
+
+            for (int i = 1; i <= count - 1; i++)
+            {
+                List<int> newNumbers = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToList();
+
+                int index = numbers.Count;
+
+                for (int j = 0; j < numbers.Count; j++)
+                {
+                    if (numbers[j] > newNumbers[0] )
+                    {
+                        index = j;
+                        break;
+                    }
+                }
+
+                numbers.InsertRange(index, newNumbers);
+
+                bool isTrim = false;
+
+                for (int j = index; j < Math.Min ((index + (newNumbers.Count)),numbers.Count - 1); j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        index = j + 1;
+                        isTrim = true;
+                        break;
+                    }
+                }
+
+                if (isTrim)
+                {
+                    numbers.RemoveRange(index, (numbers.Count - index));
+                }
+            }         
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
