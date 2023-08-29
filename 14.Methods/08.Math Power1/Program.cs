d8995a6e12f9c2bc560d6d51f520a7352414261a
Stefan Angelov <ashmodean_@abv.bg>
2023-08-29 23:50:04 +0300

    upload task 8

---
+++ 14.Methods/08.Math Power1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Math_Power1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Pow(x , y));
+        }
+
+        static double Pow(double x, double y)
+        {
+            double result = Math.Pow(x, y);
+            return result;
+        }
+    }
+}
