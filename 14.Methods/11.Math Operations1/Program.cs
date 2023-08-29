e351f4ef3f112f5cae499540932c7480ed5ff127
Stefan Angelov <ashmodean_@abv.bg>
2023-08-29 23:53:27 +0300

    upload task 11

---
+++ 14.Methods/11.Math Operations1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Math_Operations1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = int.Parse(Console.ReadLine());
+            char symbol = char.Parse(Console.ReadLine());
+            double y =int.Parse(Console.ReadLine());
+            double result = 0;
+
+            switch (symbol)
+            {
+                case '*':
+                    result = Multyply(x, y);
+                    break;
+                case '/':
+                    result = Divide(x, y);
+                    break;
+                case '+':
+                    result = Add(x, y);
+                    break;
+                case '-':
+                    result = Substract(x, y);
+                    break;
+            }
+            static double Multyply(double x, double y)
+            {
+                double result = x * y;
+                return result;
+            }
+            static double Divide(double x, double y)
+            {
+                double result = x / y;
+                return result;
+            }
+            static double Substract(double x, double y)
+            {
+                double result = x - y;
+                return result;
+            }
+            static double Add(double x, double y)
+            {
+                double result = x + y;
+                return result;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
