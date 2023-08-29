30bb390022e666b386037604fea99689f29f451c
Stefan Angelov <ashmodean_@abv.bg>
2023-08-29 23:51:12 +0300

    upload task 9

---
+++ 14.Methods/09.Greater of Two Values/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Xml;
+
+namespace Greater_of_Two_Values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string comand = Console.ReadLine();
+
+            if (comand == "int")
+            {
+                int x = int.Parse(Console.ReadLine());
+                int y = int.Parse(Console.ReadLine());
+                GetMax(x, y);
+            }
+            else if (comand == "char")
+            {
+                char x = char.Parse(Console.ReadLine());
+                char y = char.Parse(Console.ReadLine());
+                GetMax(x, y);
+            }
+            else
+            {
+                string x = Console.ReadLine();
+                string y = Console.ReadLine();
+                GetMax(x, y);
+            }
+
+            
+
+        }
+        static void GetMax(int x, int y)
+        {
+            int resulut = Math.Max(x, y);
+
+            Console.WriteLine(resulut);           
+        }
+        static void GetMax(char x, char y)
+        {
+            char resulut  = (char)Math.Max(x, y);
+
+            Console.WriteLine(resulut);
+        }
+        static void GetMax(string x, string y)
+        {
+            int result = x.CompareTo(y);
+
+            if (result > 0 ) Console.WriteLine(x);
+            else             Console.WriteLine(y);
+        }
+    }
+}
