f89872a88d776151edac489c2598a103c215fbee
Stefan Angelov <ashmodean_@abv.bg>
2023-08-29 23:52:10 +0300

    upload task 10

---
+++ 14.Methods/10.Multiply Evens by Odds/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Multiply_Evens_by_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            number = Math.Abs(number);
+
+             GetMultipleOfEvenAndOdds(GetSumOfOddDigits(number), GetSumOfEvenDigits(number));
+        }
+        static int GetSumOfOddDigits(int number)
+        {
+            int oddSum = 0;
+            
+            while (number > 0)
+            {
+                int lastDigit = number % 10;
+
+                if (lastDigit % 2 != 0) oddSum += lastDigit;
+
+                number = number / 10;
+            }
+
+            return oddSum;
+        }
+        static int GetSumOfEvenDigits(int number)
+        {
+            int evenSum = 0;
+
+            while (number > 0)
+            {
+                int lastDigit = number % 10;
+
+                if (lastDigit % 2 == 0) evenSum += lastDigit;
+
+                number = number / 10;
+            }
+
+            return evenSum;
+        }
+        static void GetMultipleOfEvenAndOdds(int oddSum, int evenSum)
+        {
+            int result = oddSum * evenSum;
+            Console.WriteLine(result);
+        }
+    }
+}
