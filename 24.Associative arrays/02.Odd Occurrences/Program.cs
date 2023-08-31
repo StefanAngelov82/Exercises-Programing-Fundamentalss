84a7718f275a3f4fcf374260b2569f76b85eff09
Stefan Angelov <ashmodean_@abv.bg>
2023-08-31 22:09:51 +0300

    upload task 02

---
+++ 24.Associative arrays/02.Odd Occurrences/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .ToLower()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+           foreach (string word in words)
+           {
+                if (!result.ContainsKey(word))
+                {
+                    result[word] = 0;
+                }
+
+                result[word]++;
+           }
+
+           List<string> oddWards = new List<string>();
+
+           foreach (var word in result)
+           {
+                if (word.Value % 2 != 0)
+                {
+                    oddWards.Add(word.Key);
+                }               
+           }
+
+            Console.WriteLine(string.Join(", ", oddWards));
+        }
+    }
+}
