d8f9b7fb17724d54cdbdda9f172b02b8d689b7ab
Stefan Angelov <ashmodean_@abv.bg>
2023-08-31 21:38:29 +0300

    upload task 01

---
+++ 24.Associative arrays/01.Count Real Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Count_Real_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+
+            SortedDictionary<double, int> realnumbers = new SortedDictionary<double, int>();
+            
+            foreach (double number in numbers)
+            {
+                if (!realnumbers.ContainsKey(number))
+                {
+                    realnumbers[number] = 0;
+                }
+
+                realnumbers[number]++;
+            }
+
+            foreach (var item in realnumbers)
+            {
+                double number = item.Key;
+                int count = item.Value;
+
+                Console.WriteLine($"{number} -> {count}");
+            }
+        }
+    }
+}
