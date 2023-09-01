eb23ad23bd23bc9c551a2e5c86f6562742f4de61
Stefan Angelov <ashmodean_@abv.bg>
2023-09-01 22:12:59 +0300

    upload task 5

---
+++ 24.Associative arrays/05.Mixed Phones/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mixed_Phones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, long> phoneBook = new SortedDictionary<string, long>();
+
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "Over")
+            {
+                string[] tokens = input.Split(" : ");
+
+                long result;
+
+                if (!long.TryParse(tokens[0], out result))
+                {
+                    phoneBook[tokens[0]] = long.Parse(tokens[1]);
+                }
+                else
+                {
+                    phoneBook[tokens[1]] = result;
+                }
+            }
+
+            foreach (var item in phoneBook)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
