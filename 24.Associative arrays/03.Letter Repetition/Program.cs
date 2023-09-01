06083f6462bafdc64a493ca098295adaa79f17dc
Stefan Angelov <ashmodean_@abv.bg>
2023-09-01 18:58:43 +0300

    upload task 3

---
+++ 24.Associative arrays/03.Letter Repetition/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Letter_Repetition
+{
+    internal class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                string text = Console.ReadLine();
+
+                char[] chars = text.ToCharArray();
+
+                Dictionary<char, int> result = new Dictionary<char, int>();
+
+                foreach (char symbol in chars)
+                {
+                    if (!result.ContainsKey(symbol))
+                    {
+                        result[symbol] = 0;
+                    }
+
+                    result[symbol]++;
+                }
+
+                foreach (var kvp in result)
+                {
+                    Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+                }
+
+            }
+       
+    }
+}
