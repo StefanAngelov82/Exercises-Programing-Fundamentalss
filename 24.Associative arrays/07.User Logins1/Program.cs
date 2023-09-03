5ff11a3af680b48a36b154d865c917fc7cc49e97
Stefan Angelov <ashmodean_@abv.bg>
2023-09-03 09:38:49 +0300

    upload task 07

---
+++ 24.Associative arrays/07.User Logins1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace User_Logins1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> passwordLog = new Dictionary<string, string>();
+
+            string command = String.Empty;
+            int failLog = 0;
+
+            while ((command = Console.ReadLine()) != "login")
+            {
+                string name = CommandArg(command, 0);
+                string pass = CommandArg(command, 1);
+
+                passwordLog[name] = pass;
+            }
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string name = CommandArg(command, 0);
+                string pass = CommandArg(command, 1);
+
+                if (passwordLog.ContainsKey(name) && passwordLog.ContainsValue(pass))
+                {
+                    Console.WriteLine($"{name}: logged in successfully");
+                }
+                else
+                {
+                    Console.WriteLine($"{name}: login failed");
+                    failLog++;
+                }               
+            }
+
+            Console.WriteLine($"unsuccessful login attempts:{failLog}");
+
+        }
+        static string CommandArg(string command, int arg)
+        {
+            string[] commandArg = command.Split(" -> "); 
+
+            if (arg == 0)   return commandArg[0];
+            else            return commandArg[1];
+        }
+    }
+}
