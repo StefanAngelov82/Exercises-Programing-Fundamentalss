2cce4416a209277d0a60183d44eee29e34f915ef
Stefan Angelov <ashmodean_@abv.bg>
2023-09-05 17:54:00 +0300

    upload task 11

---
+++ 24.Associative arrays/11.Group Continents, Countries/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Group_Continents__Countries_and_Cities
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, SortedDictionary<string, SortedSet<string>>> statistic = new SortedDictionary<string, SortedDictionary<string, SortedSet<string>>>();
+
+            int caunt = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < caunt; i++)
+            {
+                string[] inputArg = Console.ReadLine().Split();
+
+                string continent = inputArg[0];
+                string country = inputArg[1];
+                string city = inputArg[2];
+
+                if (!statistic.ContainsKey(continent))
+                {
+                    statistic[continent] = new SortedDictionary<string, SortedSet<string>>();
+                }
+
+                if (!statistic[continent].ContainsKey(country))
+                {
+                    statistic[continent][country] = new SortedSet<string>();
+                }
+
+                statistic[continent][country].Add(city);
+            }
+
+            foreach (var continent in statistic.Keys)
+            {
+                Console.WriteLine($"{continent}:");
+
+                foreach (var countrycity in statistic[continent])
+                {
+                    Console.WriteLine($"  {countrycity.Key} -> {string.Join(", ", countrycity.Value)}");
+                }
+            }
+        }
+    }
+}
