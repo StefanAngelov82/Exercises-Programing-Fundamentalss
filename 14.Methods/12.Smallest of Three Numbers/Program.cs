457d2094f67937720949e7173a42b91b0708685c
Stefan Angelov <ashmodean_@abv.bg>
2023-09-07 22:24:49 +0300

    upload task 12

---
+++ 14.Methods/12.Smallest of Three Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Smallest_of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+
+            MInValue(x, y, z);
+        }
+
+        static void MInValue(int x, int y, int z)
+        {
+            int minvalue = int.MaxValue;
+
+            if (x < minvalue) minvalue = x;
+            if (y < minvalue) minvalue = y;
+            if (z < minvalue) minvalue = z;
+
+            Console.WriteLine(minvalue);
+        }
+    }
+}
