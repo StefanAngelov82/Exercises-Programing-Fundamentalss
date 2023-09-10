6369a04be71e24bee92dd8535a9d51165312b0f5
Stefan Angelov <ashmodean_@abv.bg>
2023-09-10 23:31:44 +0300

    upload task 21

---
+++ 24.Associative arrays/21.Short Words Sorted/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Short_Words_Sorted
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine()
+                .ToLower()
+                .Split(new char[] {' ','.',',',';',':','(',')','[',']','\"','\'','\\', '/','!','?'}, StringSplitOptions.RemoveEmptyEntries)
+                .Distinct()
+                .Where(x => x.Length < 5)
+                .OrderBy(x => x)
+                .ToList();
+
+            Console.WriteLine(string.Join(", ", list));
+        }
+    }
+}
