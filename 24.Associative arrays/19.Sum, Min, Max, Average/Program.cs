1a532bdd07795aa96b0237e1b0082b508760161e
Stefan Angelov <ashmodean_@abv.bg>
2023-09-10 18:59:14 +0300

    upload task 19

---
+++ 24.Associative arrays/19.Sum, Min, Max, Average/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sum__Min__Max__Average
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count =int.Parse(Console.ReadLine());
+
+            int[] arr = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                arr[i] =int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"Sum = {arr.Sum()}");
+            Console.WriteLine($"Min = {arr.Min()}");
+            Console.WriteLine($"Max = {arr.Max()}");
+            Console.WriteLine($"Average = {arr.Average()}");
+        }
+    }
+}
