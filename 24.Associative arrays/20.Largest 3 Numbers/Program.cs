e3856005cc21ae00b58e3e8956d5d0f3f74d805b
Stefan Angelov <ashmodean_@abv.bg>
2023-09-10 19:32:39 +0300

    Upload task 20

---
+++ 24.Associative arrays/20.Largest 3 Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            Console.WriteLine(string.Join(" ", list.OrderByDescending(x => x).Take(3)));
+            
+        }
+    }
+}
