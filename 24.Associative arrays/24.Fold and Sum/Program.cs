82a62f5d062d6d2fd0a6e2df3818ea46cc94dfdd
Stefan Angelov <ashmodean_@abv.bg>
2023-09-11 21:42:04 +0300

    upload task 24

---
+++ 24.Associative arrays/24.Fold and Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+
+namespace Fold_and_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers =Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            int counter = numbers.Length / 4;
+
+            int[] first = (numbers.Take(counter).Reverse()).Concat(numbers.Reverse().Take(counter)).ToArray();
+            int[] second = numbers.Skip(counter).Take(2 * counter).ToArray();
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                Console.Write($"{first[i] + second[i]} ");
+            }
+
+            //Console.WriteLine(String.Join(" ", first ));
+            //Console.WriteLine(String.Join(" ", second ));
+        }
+    }
+}
