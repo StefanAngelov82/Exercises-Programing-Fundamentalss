0c094e03af014fa605be82504b98eea488d610d8
Stefan Angelov <ashmodean_@abv.bg>
2023-09-11 22:41:32 +0300

    upload task 25

---
+++ 24.Associative arrays/25.Registered Users/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Registered_Users
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var registerUsers = new Dictionary<string, DateTime>();
+
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] inputArg = input
+                    .Split(new string[] { " -> "}, StringSplitOptions.RemoveEmptyEntries);
+
+                string name = inputArg[0];
+                string data = inputArg[1];
+
+                DateTime date = DateTime.ParseExact(data, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+                registerUsers.Add(name, date);
+            }           
+
+            foreach (var kvp in registerUsers.OrderByDescending(x => x.Value).Reverse().TakeLast(5))
+            {
+                Console.WriteLine($"{kvp.Key}");
+            }
+        }
+    }
+}
