9d662a1c03703bf05b3228a47ebcacc3dced1d78
Stefan Angelov <ashmodean_@abv.bg>
2023-09-16 15:34:55 +0300

    upload task 31

---
+++ 24.Associative arrays/31.Array Data/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace Array_Data
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();             
+
+            string command = Console.ReadLine();
+
+            switch (command)
+            {
+                case "Min":
+
+                    Console.WriteLine(string.Join(" ", numbers.Where(x => x > numbers.Average()).Min()));
+
+                    break;
+                case "Max":
+
+                    Console.WriteLine(string.Join(" ", numbers.Where(x => x > numbers.Average()).Max()));
+
+                    break;
+                case "All":
+
+                    Console.WriteLine(string.Join(" ", numbers.Where(x => x > numbers.Average()).OrderBy(x => x)));
+
+                    break;
+            }
+        }
+    }
+}
