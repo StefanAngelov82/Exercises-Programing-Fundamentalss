2f0e4d1f812bd756c650b999aba2a46d38531196
Stefan Angelov <ashmodean_@abv.bg>
2023-09-16 19:46:38 +0300

    upload task 33

---
+++ 24.Associative arrays/33.Camping5/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Camping5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> peoplesData = new Dictionary<string, Dictionary<string, int>>(); 
+
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] inputArg = input
+                    .Split(' ')
+                    .ToArray();
+
+                string name  = inputArg[0];
+                string carModel = inputArg[1];
+                int nights = int.Parse(inputArg[2]);
+
+                if (!peoplesData.ContainsKey(name))
+                {
+                    peoplesData[name] = new Dictionary<string, int>();
+                }
+
+                if (!peoplesData[name].ContainsKey(carModel))
+                {
+                    peoplesData[name][carModel] = nights;
+                }                
+            }
+
+            peoplesData = peoplesData
+                .OrderByDescending(x => x.Value.Count)
+                .ThenBy(x => x.Key.Length)
+                .ToDictionary(x => x.Key, y => y.Value);
+
+            foreach (var name in peoplesData.Keys)
+            {
+                Console.WriteLine($"{name}: {peoplesData[name].Count}");
+
+                foreach (var kvp in peoplesData[name])
+                {
+                    Console.WriteLine($"***{kvp.Key}");
+                }
+
+                Console.WriteLine($"Total stay: {peoplesData[name].Values.Sum()} nights");
+                Console.WriteLine(new string('-', 50));
+            }
+        }
+    }
+}
