b2f4e6ecf3e17133ddca103f7ead3dae6e2a03cf
Stefan Angelov <ashmodean_@abv.bg>
2023-09-16 18:01:50 +0300

    upload task 32

---
+++ 24.Associative arrays/32.String Decryption/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace String_Decryption
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] filters = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+           List<int> word = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .Where(x => x > 59 && x < 91)
+                .ToList();
+
+            int[] result = word.Skip(filters[0]).Take(filters[1]).ToArray();
+
+            string print = String.Empty;
+
+            foreach (var letter in result)
+            {
+                print +=(char)letter;
+            }
+            Console.WriteLine(print);
+
+
+
+
+
+
+
+
+        }
+    }
+}
