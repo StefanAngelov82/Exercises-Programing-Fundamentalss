21370712e37f0800ceb9d005f8e9d79e1e239e7e
Stefan Angelov <ashmodean_@abv.bg>
2023-09-28 21:52:59 +0300

    upload task

---
+++ 27.ObjectsAndClasses/3.Big Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace Big_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            BigInteger P = 1;
+
+            for (int i = x; i >= 1; i--)
+            {
+               P *= i;
+            }
+
+            Console.WriteLine(P);
+            
+
+        }
+    }
+}
