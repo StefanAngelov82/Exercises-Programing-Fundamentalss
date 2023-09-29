34ad88f3a35671b368f26687531d970d7e644d9a
Stefan Angelov <ashmodean_@abv.bg>
2023-09-29 07:04:03 +0300

    upload task

---
+++ 27.ObjectsAndClasses/1.Day of Week/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Data;
+using System.Globalization;
+
+namespace Day_of_Week
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int[] dateArg = Console.ReadLine().Split('-').Select(int.Parse).ToArray();
+
+            DateTime result = new DateTime(dateArg[2], dateArg[1], dateArg[0]);
+
+            Console.WriteLine($"{result.DayOfWeek}");
+
+            //DateTime result = DateTime.ParseExact(Console.ReadLine(), "d-M-yyyy", CultureInfo.InvariantCulture);
+
+            Console.WriteLine(result.DayOfWeek);
+        }
+    }
+}
