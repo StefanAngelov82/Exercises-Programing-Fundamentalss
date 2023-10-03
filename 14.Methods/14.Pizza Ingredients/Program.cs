c82921b8dd5f98a7f54a5f3fff9801d34e467aa4
Stefan Angelov <ashmodean_@abv.bg>
2023-10-03 23:38:20 +0300

    upload task

---
+++ 14.Methods/14.Pizza Ingredients/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+
+namespace Pizza_Ingredients
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ingredients = Console.ReadLine().Split();
+            int x = int.Parse(Console.ReadLine());
+
+            string[] used = new string[10];
+
+            int counter = 0;
+
+            for (int i = 0; i < ingredients.Length; i++)
+            {
+                if (ingredients[i].Length == x)
+                {
+                    used[counter] = ingredients[i];
+                    counter++;
+                    Console.WriteLine($"Adding {ingredients[i]}.");
+                }
+                if (counter == 10)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine($"Made pizza with total of {counter} ingredients.");
+
+            Console.Write("The ingredients are: ");
+            Console.Write(string.Join(", ", used.Where(x => x != null)));
+            Console.WriteLine(".");
+
+        }
+    }
+}
