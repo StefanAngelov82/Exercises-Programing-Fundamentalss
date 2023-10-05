be7eded51b0e7537d5be521c23a0aea3ca98c7eb
Stefan Angelov <ashmodean_@abv.bg>
2023-10-05 19:30:40 +0300

    upload task 15

---
+++ 14.Methods/15.Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+               .Split(' ')
+               .Select(int.Parse).OrderByDescending(x => x)
+               .ToList();
+
+            double average = numbers.Average();
+
+            int count = 0;
+
+            foreach (int number in numbers)
+            {
+                if (number == average)
+                {
+                    Console.WriteLine("No");
+                    return;
+                }
+
+                if (number > average && count < 5)
+                {
+                    Console.Write($"{number} ");
+                    count++;
+                }
+            }
+    }
+}
