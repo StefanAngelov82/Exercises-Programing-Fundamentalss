ef16cd69bd2627d4fe5903225e96611e02f4fd01
Stefan Angelov <ashmodean_@abv.bg>
2023-10-06 18:19:20 +0300

    upload task 42

---
+++ 17.Lists/42.Integer Insertion/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Integer_Insertion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                 .Split(' ')
+                 .Select(int.Parse)
+                 .ToList();
+
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                
+                int index = (int)input[0] - 48;
+                int number = int.Parse(input);
+
+                numbers.Insert(index, number);
+            }
+
+            Console.WriteLine(string.Join(" ",numbers));
+        }
+    }
+}
