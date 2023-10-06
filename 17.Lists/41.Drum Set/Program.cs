b9599474361cc9bc65c433d1b79fa13713428da3
Stefan Angelov <ashmodean_@abv.bg>
2023-10-06 17:18:16 +0300

    upload task 41

---
+++ 17.Lists/41.Drum Set/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Drum_Set
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double savings = double.Parse(Console.ReadLine());
+
+            List<int> initialQualityDrums = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> currentQualityDrums = new List<int>(initialQualityDrums); 
+
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "Hit it again, Gabsy!")
+            {
+                int hitPower = int.Parse(input);
+
+                for (int i = 0; i < currentQualityDrums.Count ; i++)
+                {
+                    currentQualityDrums[i] -= hitPower;
+
+                    if (currentQualityDrums[i] <= 0)
+                    {
+                        DrrumOperation(initialQualityDrums, currentQualityDrums,  ref savings, ref i);
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", currentQualityDrums));
+            Console.WriteLine($"Gabsy has {savings:F2}lv.");
+        }
+
+        private static void DrrumOperation(List<int> initialQualityDrums, List<int> currentQualityDrums,  ref double savings,  ref int index)
+        {
+            double newDrumprice = initialQualityDrums[index] * 3;
+
+            if (savings >= newDrumprice )
+            {
+                savings -= newDrumprice;
+
+                currentQualityDrums[index] = initialQualityDrums[index];
+            }
+            else
+            {
+                initialQualityDrums.RemoveAt(index);
+                currentQualityDrums.RemoveAt(index);
+                index--;
+            }
+        }
+    }
+}
