632ea51aeafef5ff15a6561fb8dae3564eeb3138
Stefan Angelov <ashmodean_@abv.bg>
2023-10-06 21:46:03 +0300

    upload task 44

---
+++ 17.Lists/44.Ununion Lists/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ununion_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbersData = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int j = 0; j < input.Length; j++)
+                {
+                    bool isNotExist = true;
+
+                    for (int k = 0; k < numbersData.Count; k++)
+                    {
+                        if (input[j] == numbersData[k])
+                        {
+                            numbersData.Remove(numbersData[k]);
+                            k--;
+
+                            isNotExist = false;
+                        }                         
+                    }
+
+                    if (isNotExist) numbersData.Add(input[j]);                    
+                }
+            }
+
+            numbersData.Sort();
+
+            Console.WriteLine(string.Join(" ", numbersData));
+        }
+    }
+}
