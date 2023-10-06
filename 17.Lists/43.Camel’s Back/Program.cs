ddbecefc32ac375dc27d66960f0a4612f127177c
Stefan Angelov <ashmodean_@abv.bg>
2023-10-06 19:26:44 +0300

    upload tas 43

---
+++ 17.Lists/43.Camel’s Back/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Camel_s_Back
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> houses = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            int camelNumber = int.Parse(Console.ReadLine());
+
+            int decrementor = (houses.Count - camelNumber) / 2;
+            
+            for (int i = 1; i <= decrementor ; i++)
+            {
+                houses.RemoveAt(0);               
+                houses.RemoveAt(houses.Count - 1);
+            }
+
+            if (decrementor == 0)
+            {
+                Console.Write("already stable: ");
+                Console.WriteLine(string.Join(" ", houses));
+            }
+            else
+            {
+                Console.WriteLine($"{decrementor} rounds");
+                Console.WriteLine(string.Join(" ", houses));
+            }
+
+            
+        }
+    }
+}
