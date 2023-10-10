d87596024133e09cf5cbbc930a75390563b38811
Stefan Angelov <ashmodean_@abv.bg>
2023-10-10 18:18:36 +0300

    upload task 16

---
+++ 14.Methods/16.Computer Store/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Computer_Store
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = String.Empty;
+
+            double totalSum = 0;
+
+            while ((input = Console.ReadLine()) != "special" && input != "regular")
+            {
+                double currentPrice = double.Parse(input);
+
+                if (currentPrice < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                    continue;
+                }
+
+                totalSum += currentPrice;
+            }
+
+            if (totalSum == 0)
+            {
+                Console.WriteLine("Invalid order!");
+                return;
+            }
+
+            double tax = totalSum * 0.2;
+
+            Console.WriteLine("Congratulations you've just bought a new computer!");
+            Console.WriteLine($"Price without taxes: {totalSum:F2}$");
+            Console.WriteLine($"Taxes: {tax:F2}$");
+
+            Console.WriteLine("-----------");
+
+            if (input == "regular")
+            {
+                Console.WriteLine($"Total price: {totalSum + tax:F2}$");
+            }
+            else
+            {
+                Console.WriteLine($"Total price: {(totalSum + tax) * 0.9:F2}$");
+            }
+        }
+    }
+}
