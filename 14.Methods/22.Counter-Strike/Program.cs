df952d4fd8934728f78152129f17ecda50f6340c
Stefan Angelov <ashmodean_@abv.bg>
2023-10-15 18:36:39 +0300

    upload task 22

---
+++ 14.Methods/22.Counter-Strike/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Counter_Strike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialEnegry = int.Parse(Console.ReadLine());
+
+
+            string input = String.Empty;
+
+            int winCount = 0;
+
+
+            while (initialEnegry >= 0 && (input = Console.ReadLine()) != "End of battle")
+            {
+                int distance = int.Parse(input);
+
+                if (distance <= initialEnegry)
+                {
+                    winCount++;
+
+                    initialEnegry -= distance;
+                }
+                else
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {winCount} won battles and {initialEnegry} energy");
+                    return;
+                }
+
+                if (winCount % 3 == 0)
+                {
+                    initialEnegry += winCount;
+                }
+            }
+
+            Console.WriteLine($"Won battles: {winCount}. Energy left: {initialEnegry}");
+
+        }
+    }
+}
