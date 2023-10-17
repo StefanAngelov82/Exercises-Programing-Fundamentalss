0dda376ec126c90bb704cdc24bbe518e42181571
Stefan Angelov <ashmodean_@abv.bg>
2023-10-17 23:05:00 +0300

    UPLOAD TAS 24

---
+++ 14.Methods/24.Test6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Test6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { int i, j;
+            int[] x = { 1, 2, 3, 4, 5};
+            int[] y = x[0..3];
+            int[] z = x[3..];
+            int[] w = new int[x.Length];
+
+            for (i = 0; i < y.Length; i++)
+            {
+                w[i] = y[i];
+            }
+            
+           for (j  = i; j < x.Length; j++)
+            {
+                w[j] = z[j - i];
+            }
+
+            foreach (var item in w)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
