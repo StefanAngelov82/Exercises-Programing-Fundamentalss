e000f7edc9c76d6e2b2833486ce00dcc2dd1d28e
Stefan Angelov <ashmodean_@abv.bg>
2023-10-22 13:40:41 +0300

    upload task 27

---
+++ 14.Methods/27.Phone Shop/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Phone_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> phones = Console.ReadLine()
+                .Split(", ")
+                .ToList();
+
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] inputArg = input.Split(new string[] {" - ",":"}, StringSplitOptions.RemoveEmptyEntries);
+
+                switch (inputArg[0])
+                {
+                    case "Add":
+
+                        if (!phones.Contains(inputArg[1]))
+                        {
+                            phones.Add(inputArg[1]);
+                        } 
+
+                        break;
+
+                    case "Remove":
+
+                        phones.Remove(inputArg[1]);
+
+                        break;
+
+                    case "Bonus phone":
+
+                        int index = phones.IndexOf(inputArg[1]);
+
+                        if (index >= 0)
+                        {
+                            phones.Insert(index + 1, inputArg[2]);
+                        }
+
+                        break;
+                    case "Last":
+
+                        if (phones.Contains(inputArg[1]))
+                        {
+                            phones.Add(inputArg[1]);
+                            phones.Remove(inputArg[1]);
+
+                        }
+
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", phones));
+        }
+    }
+}
