5c91508158a3fd90509d8aaa40fa90097e898436
Stefan Angelov <ashmodean_@abv.bg>
2023-10-23 21:17:24 +0300

    upload test

---
+++ 14.Methods/30.test/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string texts = "Maro";
+            StringBuilder sb = new StringBuilder();
+            StringBuilder sb1 = new StringBuilder();
+
+            
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            
+
+            char[] chars = texts.ToCharArray();
+
+            Array.Reverse(chars);
+
+            Console.WriteLine(new string(chars));
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"First attempt : {stopwatch.Elapsed}");
+
+            stopwatch.Reset();
+
+            ///////////////////////////////////////////////////
+            ///
+
+
+            stopwatch.Start();
+
+
+
+            for (int i = texts.Length - 1; i >= 0; i--)
+            {
+                sb.Append(texts[i]); 
+            }
+
+            Console.WriteLine(sb.ToString());
+
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"First attempt1 : {stopwatch.Elapsed}");
+
+
+            ///////////////////////////
+            ///
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            texts = texts.Reverse().ToString();
+
+            Console.WriteLine(texts);
+            Console.WriteLine($"First attempt2 : {stopwatch.Elapsed}");
+
+
+            //////////////////////
+            ///
+            stopwatch.Reset();
+            stopwatch.Start();
+
+            
+        }
+    }
+}
