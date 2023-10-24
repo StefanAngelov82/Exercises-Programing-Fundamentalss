b1ac0c4aa10ed39396d70cc175c87c5a46e3ed89
Stefan Angelov <ashmodean_@abv.bg>
2023-10-24 19:04:48 +0300

    upload task 31

---
+++ 14.Methods/31.Text filter/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Text_filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banwords = Console.ReadLine()
+                .Split(", ")
+                .ToArray();
+
+            string Text = Console.ReadLine();
+
+
+
+            for (int i = 0; i < banwords.Length; i++)
+            {
+                 Text = Text.Replace(banwords[i], new string('*', banwords[i].Length));
+            }
+
+            Console.WriteLine(Text);
+        }
+    }
+}
