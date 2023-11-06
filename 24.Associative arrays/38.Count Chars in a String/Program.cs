aeeb83ff4e247895060a79b5cb39e699ba1f03f9
Stefan Angelov <ashmodean_@abv.bg>
2023-11-06 20:59:25 +0200

    upload task 38

---
+++ 24.Associative arrays/38.Count Chars in a String/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Count_Chars_in_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string text = Console.ReadLine();
+
+            Dictionary<char, int> data = new Dictionary<char, int>();
+
+            foreach (var symbol in text.Where(x => x is not ' '))
+            {
+                if (!data.ContainsKey(symbol))
+                {
+                    data[symbol] = 0;
+                }
+
+                data[symbol]++;
+            }
+
+
+            foreach (var kvp in data)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
