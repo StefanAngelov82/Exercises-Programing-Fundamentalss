8443f0fbf66b9c13026b3076fc7c91a3c9a06c13
Stefan Angelov <ashmodean_@abv.bg>
2023-11-06 21:28:33 +0200

    upload task 39

---
+++ 24.Associative arrays/39.A Miner Task/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, long> data = new Dictionary<string, long>();
+
+            string input = String.Empty;
+            String input2 = String.Empty;           
+
+            while ((input = Console.ReadLine()) != "stop")
+            {
+                input2 = Console.ReadLine();
+
+                if (!data.ContainsKey(input))
+                {
+                    data[input] = 0;
+                }
+
+                data[input] += long.Parse(input2);
+            }
+
+            foreach ( var kvp in data)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
