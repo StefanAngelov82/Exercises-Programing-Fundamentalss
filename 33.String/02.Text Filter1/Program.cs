40b2a37edaeb4ce7abdb3ec31f07c4c464e6656d
Stefan Angelov <ashmodean_@abv.bg>
2023-11-12 20:57:17 +0200

    upload

---
+++ 33.String/02.Text Filter1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Text_Filter1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] filters = Console.ReadLine().Split(", ");
+
+            string text = Console.ReadLine();
+
+           foreach (string filter in filters)
+           {
+              text = text.Replace(filter, new string('*', filter.Length));
+           }
+
+            Console.WriteLine(text);
+        }
+
+    }
+}
