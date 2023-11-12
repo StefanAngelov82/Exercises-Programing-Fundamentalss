66f47d17071081f233e8408f3c54b0e6247a2566
Stefan Angelov <ashmodean_@abv.bg>
2023-11-12 20:59:12 +0200

    upload task 1

---
+++ 33.String/01.Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text1 = Console.ReadLine();
+            string text2 = Console.ReadLine();
+
+            int index = 0;
+
+            while (text2.Contains(text1))
+            {               
+                text2 = text2.Remove(text2.IndexOf(text1), text1.Length);
+            }
+
+            Console.WriteLine(text2);
+        }
+    }
+}
