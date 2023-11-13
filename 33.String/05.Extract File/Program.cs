1632fa266faf15e32890e561005d1df3d83ddf52
Stefan Angelov <ashmodean_@abv.bg>
2023-11-13 21:01:21 +0200

    upload task 5

---
+++ 33.String/05.Extract File/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Extract_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int indexSlash = input.LastIndexOf('\\');
+            int indexDot = input.LastIndexOf(".");
+
+            int lenght = indexDot - (indexSlash + 1);
+
+            string fileName = input.Substring(indexSlash + 1, lenght);
+            string extension = input.Substring(indexDot + 1);
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {extension}");
+        }
+    }
+}
