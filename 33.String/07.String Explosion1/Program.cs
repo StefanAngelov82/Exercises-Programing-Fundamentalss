6bc059d9a290047f530a9d5b8d11af8e5c8b7726
Stefan Angelov <ashmodean_@abv.bg>
2023-11-14 20:22:16 +0200

    upload task 07

---
+++ 33.String/07.String Explosion1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace String_Explosion1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            int counter = 0;            
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] != '>' && counter == 0 )
+                {
+                    sb.Append(text[i]);
+                }
+                else if (text[i] == '>')
+                {
+                    if (counter != 0) counter--;
+
+                    sb.Append(text[i]);
+                    counter += int.Parse(text[i + 1].ToString());                    
+                }                
+                else
+                {
+                    counter--;
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
