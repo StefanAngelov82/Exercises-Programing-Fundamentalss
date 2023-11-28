84b668d04a32d20c16ef803760202f414ebff9ce
Stefan Angelov <ashmodean_@abv.bg>
2023-11-28 19:37:41 +0200

    upload task 10

---
+++ 33.String/10.Furniture/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Furniture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = String.Empty;
+
+            Dictionary<string, double> data = new Dictionary<string, double>();
+
+            while ((input = Console.ReadLine()) is not "Purchase")
+            {
+                string pattern = @">>(?<furniture>\w+)<<(?<price>[\d.]+)!(?<quantity>\d+)";
+
+                Match matches = Regex.Match(input, pattern); 
+
+                if (matches.Success)
+                {
+                    string furniture = matches.Groups[1].Value;
+                    double moneySpend = double.Parse(matches.Groups[2].Value) * double.Parse(matches.Groups[3].Value);
+
+                    if (!data.ContainsKey(furniture))
+                    {
+                        data[furniture] = 0;
+                    }
+
+                    data[furniture] += moneySpend;
+                }                
+            }
+
+            double totalprice = 0;
+
+            Console.WriteLine("Bought furniture:");
+
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.Key);
+                totalprice += item.Value;
+            }
+
+            Console.WriteLine($"Total money spend: {totalprice:F2}");
+        }
+    }
+}
