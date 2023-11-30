cbf4ab7f285025599f133acc27ef3c05fb4ec175
Stefan Angelov <ashmodean_@abv.bg>
2023-11-30 22:03:51 +0200

    upload task 15

---
+++ 33.String/15.Extract Emails1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.WebSockets;
+using System.Text.RegularExpressions;
+
+namespace Extract_Emails1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"(?<=\s)[A-Za-z0-9]+[\.\-_]*[A-Za-z0-9]*@[A-Za-z0-9]+[A-Za-z0-9\.\-_]*[\.][A-Za-z0-9]+";
+           
+
+            foreach (Match match in Regex.Matches(input, pattern))
+            {
+                Console.WriteLine(match.Groups[0].Value);
+            }
+        }
+    }
+}
