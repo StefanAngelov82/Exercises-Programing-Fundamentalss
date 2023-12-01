782bce14df764b75fc9bf4c137485346c943e002
Stefan Angelov <ashmodean_@abv.bg>
2023-12-01 16:30:58 +0200

    task 17 upload

---
+++ 33.String/17.Rage Quit/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Rage_Quit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text  = Console.ReadLine();           
+
+            string pattern = @"(?<symbols>[\D]+)(?<digit>[\d]+)";
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (Match match in Regex.Matches(text, pattern))
+            {
+                int N = int.Parse(match.Groups[2].Value);
+
+                string symbols = (match.Groups[1].Value).ToUpper();
+
+                for (int i = 0; i < N; i++)
+                {
+                    sb.Append(symbols);
+                }               
+            }
+
+            Console.WriteLine($"Unique symbols used: {sb.ToString().Distinct().Count()}");
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
