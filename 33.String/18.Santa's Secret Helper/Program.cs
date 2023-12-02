766e671d0d9f03e9b3c6ce41eec166346917fd13
Stefan Angelov <ashmodean_@abv.bg>
2023-12-02 19:14:03 +0200

    upload task 18

---
+++ 33.String/18.Santa's Secret Helper/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Santa_s_Secret_Helper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            string text = String.Empty;
+
+            List<string> list = new List<string>();
+
+            while ((text = Console.ReadLine()) != "end")
+            {
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var symbol in text)
+                {
+                    sb.Append((char)(symbol - N));
+                }
+
+                text = sb.ToString();
+
+                string pattern = @"@(?<name>[A-Za-z]+)[^@\-!:>]*!(?<grade>[G|N])!";
+
+                foreach (Match match in Regex.Matches(text, pattern))
+                {
+                    if (match.Groups["grade"].Value == "G")
+                    {
+                        list.Add(match.Groups["name"].Value);
+                    }
+                }                
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, list));
+        }
+    }
+}
