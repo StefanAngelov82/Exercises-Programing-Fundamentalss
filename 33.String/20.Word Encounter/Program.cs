e5e2a76ffe240fa8a9f6dd461187fc82143d2543
Stefan Angelov <ashmodean_@abv.bg>
2023-12-02 22:56:11 +0200

    upload task 20

---
+++ 33.String/20.Word Encounter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Word_Encounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = Console.ReadLine();
+
+            char symbol = key[0];
+            int N = int.Parse(key[1].ToString());
+
+            string input = String.Empty;
+
+            List<string> output = new List<string>();
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                Regex reg = new Regex(@"^[A-Z](.*)[?!.]$");
+
+                if (reg.IsMatch(input))
+                {
+                    foreach (Match match in Regex.Matches(input, @"[A-Za-z]+"))
+                    {
+                        if (match.Value.Count(x => x == symbol) == N)
+                        {
+                            output.Add(match.Value);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", output));
+        }
+    }
+}
