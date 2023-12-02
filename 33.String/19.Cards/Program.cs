8460d46db723707de7615979080a42a47a88971d
Stefan Angelov <ashmodean_@abv.bg>
2023-12-02 20:14:35 +0200

    upload task 19

---
+++ 33.String/19.Cards/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Cards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            List<string> data = new List<string>(); 
+
+            string patterh = @"(?:[2-9JQKA]|10)[SHDC]";
+
+            foreach (Match match in Regex.Matches(text, patterh))
+            {
+                data.Add(match.Value);
+            }
+
+            Console.WriteLine(string.Join(", ", data));
+        }
+    }
+}
