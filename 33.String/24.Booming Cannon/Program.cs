e904bd24355a6e8bfddb86a07633005ca9a6a5dc
Stefan Angelov <ashmodean_@abv.bg>
2023-12-03 20:06:10 +0200

    upload task 25

---
+++ 33.String/24.Booming Cannon/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace Booming_Cannon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] info = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            List<string> result = new List<string>();
+
+            int distance = info[0];
+            int damage = info[1];
+
+            string topology = Console.ReadLine();
+
+            string pattern = @"(?<=\\<<<)(?<target>.*?)(?=\\<<<|$)";
+
+            foreach (Match match in Regex.Matches(topology, pattern))
+            {
+                string victome = (match.Groups["target"].Value).Substring(distance, damage);
+
+                if (victome.Length != 0)
+                {
+                    result.Add(victome);
+                }               
+            }
+
+            Console.WriteLine(string.Join("/\\", result));
+        }
+    }
+}
