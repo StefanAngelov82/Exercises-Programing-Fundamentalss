49d7872a697d581b82280246685bd601e7ba7e96
Stefan Angelov <ashmodean_@abv.bg>
2023-12-05 22:21:39 +0200

    upload task 26

---
+++ 33.String/26.SoftUni Messages/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SoftUni_Messages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "Decrypt!")
+            {
+                int N = int.Parse(Console.ReadLine());                
+
+                string pattern = @"^(?<partOne>\d+)(?<massage>[A-Za-z]+)(?<partTwo>[^A-Za-z]+)$";
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (Match matches in Regex.Matches(input, pattern))
+                {
+                    string text = matches.Groups["massage"].Value;
+
+                    if (text.Length != N) break;                                 
+
+                    foreach(char digit in text) 
+                    {
+                        int index;
+                        bool success = int.TryParse(digit.ToString(), out index);
+
+                        if (!success) continue;
+
+                        if ((index) >= 0 && (index) < text.Length)
+                        {
+                            sb.Append(text[index]);
+                        }
+                    }
+
+                    Console.WriteLine($"{text} = {sb.ToString()}");
+                }                
+            }
+        }
+    }
+}
