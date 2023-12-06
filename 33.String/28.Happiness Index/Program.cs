9359cbe4b675bf54cf04348711bf45db61c80397
Stefan Angelov <ashmodean_@abv.bg>
2023-12-06 20:40:39 +0200

    upload task 28

---
+++ 33.String/28.Happiness Index/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Happiness_Index
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string positivePattern = @"(?<positive>[\(\*c\[][:|;]|[:|;][\)D\*\}\]])";
+            string negativPattern = @"(?<positive>[\)D\]][:|;]|[:|;][\(\{\[c])";
+
+            Regex regPositive  = new Regex(positivePattern);
+            Regex regNegative = new Regex(negativPattern);
+
+            int positiveCount = regPositive.Matches(input).Count();
+            int negativeCount = regNegative.Matches(input).Count();
+
+            double happinessIndex =(double) positiveCount / negativeCount;
+
+            string happiness = String.Empty;
+
+            if (happinessIndex >= 2)        happiness = ":D";
+            else if (happinessIndex > 1)    happiness = ":)";
+            else if (happinessIndex == 1)   happiness = ":|";
+            else                            happiness = ":(";            
+
+            Console.WriteLine($"Happiness index: {happinessIndex:F2} {happiness}");
+            Console.WriteLine($"[Happy count: {positiveCount}, Sad count: {negativeCount}]");
+        }
+    }
+}
