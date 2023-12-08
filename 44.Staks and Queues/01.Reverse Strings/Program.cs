57867add316a425aa5ee987f0153b2248ec8272c
Stefan Angelov <ashmodean_@abv.bg>
2023-12-08 18:33:25 +0200

    upload task 1

---
+++ 44.Staks and Queues/01.Reverse Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection.Metadata;
+using System.Text;
+
+namespace Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();   
+            
+            var stack = new Stack<char>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                stack.Push(input[i]);
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                sb.Append(stack.Pop());
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
