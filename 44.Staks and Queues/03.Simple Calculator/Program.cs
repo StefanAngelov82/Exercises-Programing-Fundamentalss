0ea03412870d7c1e65df2ece2edf0a46218d75d5
Stefan Angelov <ashmodean_@abv.bg>
2023-12-08 20:27:37 +0200

    upload task 3

---
+++ 44.Staks and Queues/03.Simple Calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection.Metadata;
+
+namespace Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            Stack<string> stack = new Stack<string>(input.Reverse());
+
+            int sum = int.Parse(stack.Pop());
+
+            while (stack.Count != 0 )
+            {                
+                string symbol = stack.Pop();
+                int number = int.Parse(stack.Pop());
+
+                if (symbol == "+")
+                {
+                    sum += number;
+                }
+                else
+                {
+                    sum -= number;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
