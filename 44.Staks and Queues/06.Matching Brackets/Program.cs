a38095517b48997e17bb5a48dde3d1e5390cc70b
Stefan Angelov <ashmodean_@abv.bg>
2023-12-09 19:13:39 +0200

    upload task 6

---
+++ 44.Staks and Queues/06.Matching Brackets/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    stack.Push(i);
+                }
+                if (input[i] == ')')
+                {
+                    int start = stack.Pop();
+                    int end = i;
+
+                    string result = input.Substring(start, end - start + 1);
+
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
