ef893acf0ab45ba6c848c14d2660174a2fe313c9
Stefan Angelov <ashmodean_@abv.bg>
2023-12-10 22:26:51 +0200

    upload task 11

---
+++ 44.Staks and Queues/11.Reverse Numbers with a Stack/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Reverse_Numbers_with_a_Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            while (stack.Count > 0)
+            {
+                Console.Write($"{stack.Pop()} ");
+            }
+        }
+    }
+}
