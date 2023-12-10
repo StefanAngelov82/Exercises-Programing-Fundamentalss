411e9d81fe48c550e65ffbd4a2ff71b7dfcbff8d
Stefan Angelov <ashmodean_@abv.bg>
2023-12-10 17:13:03 +0200

    upload task 8

---
+++ 44.Staks and Queues/08.Print Even Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> data = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            int counter = data.Count;
+           
+            while (counter > 0)
+            {
+                if (data.Peek() % 2 == 0) data.Enqueue(data.Dequeue());                
+                else                      data.Dequeue();                
+
+                counter--;
+            }
+
+            Console.WriteLine(String.Join(", ", data));            
+
+        }
+    }
+}
