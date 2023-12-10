16395b2760d5f0d1d39e060c3110a073ec26fc31
Stefan Angelov <ashmodean_@abv.bg>
2023-12-10 11:52:42 +0200

    upload task 7

---
+++ 44.Staks and Queues/07.Hot Potato/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            int N = int.Parse(Console.ReadLine());
+
+            Queue<string> people = new Queue<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                people.Enqueue(input[i]);
+            }
+
+            int count = 1;
+            string name = String.Empty;
+
+            while (people.Count > 1)
+            {
+                if (count != N)
+                {
+                    name = people.Dequeue();
+                    people.Enqueue(name);
+
+                }
+                else
+                {
+                    name = people.Dequeue();
+                    Console.WriteLine($"Removed {name}");
+                    count = 0;
+                }
+
+                count++;
+            }
+
+            Console.WriteLine($"Last is {people.Dequeue()}");
+        }
+    }
+}
