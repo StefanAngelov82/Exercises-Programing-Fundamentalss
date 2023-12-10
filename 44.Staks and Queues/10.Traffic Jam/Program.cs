3db42e241a51b31d84caff869e4157ec26fbff90
Stefan Angelov <ashmodean_@abv.bg>
2023-12-10 22:04:48 +0200

    upload task 10

---
+++ 44.Staks and Queues/10.Traffic Jam/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Traffic_Jam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int passers = int.Parse(Console.ReadLine());
+
+            string input =String.Empty;
+            int counter = 0;
+
+            Queue<string> data = new Queue<string>();
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                if (input != "green")
+                {
+                    data.Enqueue(input);
+                }
+                else
+                {
+                    int end = data.Count;
+
+                    for (int i = 0; i < Math.Min(passers, end); i++)
+                    {
+                        Console.WriteLine($"{data.Dequeue()} passed!");
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{counter} cars passed the crossroads.");
+        }
+    }
+}
