5163fc83075298a64debecff32423e6256616fb0
Stefan Angelov <ashmodean_@abv.bg>
2023-12-10 18:03:43 +0200

    upload task 9

---
+++ 44.Staks and Queues/09.Supermarket/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> data = new Queue<string>();
+
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input != "Paid")
+                {
+                    data.Enqueue(input);
+                }
+                else
+                {
+                    Console.WriteLine(string.Join(Environment.NewLine, data));
+
+                    data.Clear();
+                }
+            }
+
+            Console.WriteLine($"{data.Count} people remaining.");
+        }
+    }
+}
