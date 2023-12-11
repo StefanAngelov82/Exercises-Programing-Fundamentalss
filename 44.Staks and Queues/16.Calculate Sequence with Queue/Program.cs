15aa1d6db8c6ab961e5cf1c9cc12da926e101628
Stefan Angelov <ashmodean_@abv.bg>
2023-12-11 19:51:50 +0200

    upload task 17

---
+++ 44.Staks and Queues/16.Calculate Sequence with Queue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Calculate_Sequence_with_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal N = decimal.Parse(Console.ReadLine());
+
+            Queue<decimal> data = new Queue<decimal>();
+
+            data.Enqueue(N);
+
+            for (int i = 1; i < 18; i++)
+            {
+                data.Enqueue(N + 1);
+                data.Enqueue(2 * N + 1);
+                data.Enqueue(N + 2);
+
+                N = data.ElementAtOrDefault(i);
+            }
+
+            Console.WriteLine(String.Join(" ",data.Take(50)));
+        }
+    }
+}
