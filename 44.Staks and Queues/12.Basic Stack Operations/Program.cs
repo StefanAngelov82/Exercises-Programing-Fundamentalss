0f95f25a92f86e10fb994864fb44d2c2ee33a827
Stefan Angelov <ashmodean_@abv.bg>
2023-12-11 11:01:23 +0200

    upload task 12

---
+++ 44.Staks and Queues/12.Basic Stack Operations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Basic_Stack_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stack = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+
+            int min = stack.Count();
+
+            for (int i = 0; i < Math.Min(min, data[1]); i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Contains(data[2]))
+            {
+                Console.WriteLine($"true");
+            }
+            else if (stack.Count > 0)
+            {
+                Console.WriteLine(stack.Min());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
