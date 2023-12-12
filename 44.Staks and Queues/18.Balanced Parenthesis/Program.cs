319f4323494d524c67dc9805761e2b77f63e8631
Stefan Angelov <ashmodean_@abv.bg>
2023-12-12 21:31:47 +0200

    upload task 18

---
+++ 44.Staks and Queues/18.Balanced Parenthesis/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Balanced_Parenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            Stack<char> one = new Stack<char>();
+
+            foreach (var symbol in text)
+            {
+                if (symbol == '(' || symbol == '{' || symbol == '[')
+                {
+                    one.Push(symbol);
+                }
+                else
+                {
+                    if (one.Count == 0)
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                    else
+                    {
+                        char symbol2 = one.Pop();
+                        string result = symbol2.ToString() + symbol.ToString();
+
+                        if (result != "()" && result != "[]" && result != "{}")
+                        {
+                            Console.WriteLine("NO");
+                            return;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("YES");
+        }
+          
+    }
+}
