40488cc3d1234d6f644a98d5aaabda6626c7b4ca
Stefan Angelov <ashmodean_@abv.bg>
2023-12-16 20:55:49 +0200

    upload task 01

---
+++ 55.HashSet and Dic/01.Parking Lot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+
+namespace Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] inputArg = input.Split(", ");
+
+                if (inputArg[0] == "IN")
+                {
+                    set.Add(inputArg[1]);
+                }
+                else
+                {
+                    set.Remove(inputArg[1]);
+                }
+            }
+
+            if (set.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                Console.WriteLine(String.Join(Environment.NewLine, set));
+            }
+
+        }
+    }
+}
