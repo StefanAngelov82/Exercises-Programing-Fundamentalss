03d68c9f542588c3f7ce9325c4a409d47a7fee29
Stefan Angelov <ashmodean_@abv.bg>
2023-12-16 22:53:42 +0200

    upload task 3

---
+++ 55.HashSet and Dic/03.Record Unique Names/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            HashSet<string> set = new HashSet<string>();
+
+            for (int i = 0; i < N; i++)
+            {
+                set.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, set));
+        }
+    }
+}
