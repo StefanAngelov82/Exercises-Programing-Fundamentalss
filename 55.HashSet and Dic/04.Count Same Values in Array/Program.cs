77c455a0e099454ca705a15ef6f9773302fe47b3
Stefan Angelov <ashmodean_@abv.bg>
2023-12-17 17:12:29 +0200

    upload task 4

---
+++ 55.HashSet and Dic/04.Count Same Values in Array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Channels;
+
+namespace Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<double, int> data = new Dictionary<double, int>();
+
+            double[] numbers = Console.ReadLine()
+                .Split()
+                .Select(double.Parse)
+                .ToArray();
+
+            foreach (double number in numbers)
+            {
+                if (!data.ContainsKey(number))
+                {
+                    data[number] = 0;
+                }
+
+                data[number]++;
+            }
+            foreach (var kvp in data)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+
+        }
+    }
+}
