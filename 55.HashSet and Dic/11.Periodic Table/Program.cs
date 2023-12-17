5bf192000c317996434d92c056190b099258c220
Stefan Angelov <ashmodean_@abv.bg>
2023-12-17 21:01:11 +0200

    upload task 11

---
+++ 55.HashSet and Dic/11.Periodic Table/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            SortedSet<string> data = new SortedSet<string>();
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                foreach (string s in input)
+                {
+                    data.Add(s);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", data));
+        }
+    }
+}
