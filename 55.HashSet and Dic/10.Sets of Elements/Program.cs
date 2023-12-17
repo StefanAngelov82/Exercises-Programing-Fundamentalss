d6ff861b6afeb13890d824360ad13c8d346353d7
Stefan Angelov <ashmodean_@abv.bg>
2023-12-17 20:40:21 +0200

    upload task 10

---
+++ 55.HashSet and Dic/10.Sets of Elements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondtSet = new HashSet<int>();
+           
+
+            for (int i = 0; i < numbers[0]; i++)
+            {
+                firstSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < numbers[1]; i++)
+            {
+                secondtSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            firstSet.IntersectWith(secondtSet);
+            
+            Console.WriteLine(string.Join(" ", firstSet));
+        }
+    }
+}
