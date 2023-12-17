276a988c540a43f9eba2a18513f0aecd79ff0c53
Stefan Angelov <ashmodean_@abv.bg>
2023-12-17 20:10:29 +0200

    upload task 9

---
+++ 55.HashSet and Dic/09.Unique Usernames/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Unique_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string name = Console.ReadLine();
+
+                set.Add(name);
+            }
+
+            foreach (string name in set)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
