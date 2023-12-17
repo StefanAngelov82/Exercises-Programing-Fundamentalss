7969bdd93e5c079e915bf063019ea4ae0e39dac3
Stefan Angelov <ashmodean_@abv.bg>
2023-12-17 18:37:06 +0200

    upload task 6

---
+++ 55.HashSet and Dic/06.Largest 3 Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse).OrderByDescending(x => x)
+                .Take(3)
+                .ToArray();
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
