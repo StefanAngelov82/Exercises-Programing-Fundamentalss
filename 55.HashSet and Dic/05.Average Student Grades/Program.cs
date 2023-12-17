783158a76b5e3e4ea34c30d456202f6e834761fd
Stefan Angelov <ashmodean_@abv.bg>
2023-12-17 18:19:36 +0200

    upload  task 05

---
+++ 55.HashSet and Dic/05.Average Student Grades/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Average_Student_Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<decimal>> studentsData = new Dictionary<string, List<decimal>>();
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] inputArg = Console.ReadLine().Split();
+
+                string name = inputArg[0];
+                decimal grade = decimal.Parse(inputArg[1]);
+
+                if (!studentsData.ContainsKey(name))
+                {
+                    studentsData[name] = new List<decimal>();
+                }
+
+                studentsData[name].Add(grade);
+            }
+
+            foreach (var student in studentsData)
+            {
+                Console.WriteLine($"{student.Key} -> {string.Join(" ", student.Value.Select(x => $"{x:F2}"))} (avg: {student.Value.Average():F2})");
+            }
+        }
+    }
+}
