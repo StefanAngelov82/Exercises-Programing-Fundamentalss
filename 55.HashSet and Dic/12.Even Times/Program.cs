734630b09c19f0ecfa04b3dd160fc4ec77c2046a
Stefan Angelov <ashmodean_@abv.bg>
2023-12-18 21:08:28 +0200

    upload task 12

---
+++ 55.HashSet and Dic/12.Even Times/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> data = new Dictionary<int, int>();
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                
+                if (!data.ContainsKey(number))
+                {
+                    data[number] = 0;
+                }
+
+                data[number]++;
+            }
+
+            foreach (var item in data.Where(x => x.Value % 2 == 0)) 
+            {
+                Console.WriteLine(item.Key);
+            }
+            
+        }
+    }
+}
