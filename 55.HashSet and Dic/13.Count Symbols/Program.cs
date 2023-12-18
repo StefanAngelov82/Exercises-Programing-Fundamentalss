4f59a5bf634c60217a3efa3bddd8b3d2bb497605
Stefan Angelov <ashmodean_@abv.bg>
2023-12-18 21:28:26 +0200

    upload task 13

---
+++ 55.HashSet and Dic/13.Count Symbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> data = new SortedDictionary<char, int>();
+
+            string text = Console.ReadLine();
+
+            foreach (char c in text)
+            {
+                if (!data.ContainsKey(c))
+                {
+                    data[c] = 0;
+                }
+
+                data[c]++;
+            }
+
+            foreach (var cj in data)
+            {
+                Console.WriteLine($"{cj.Key}: {cj.Value} time/s");
+            }
+        }
+    }
+}
