f924b29be32eddb6483cd00b8e606e51871c5f10
Stefan Angelov <ashmodean_@abv.bg>
2023-12-23 18:31:11 +0200

    upload task 20

---
+++ 55.HashSet and Dic/20.Logs Aggregator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Logs_Aggregator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, SortedDictionary<string, int>> data = new SortedDictionary<string, SortedDictionary<string, int>>();
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] inputArg = Console.ReadLine().Split(" ");
+
+                string name = inputArg[1];
+                string IP = inputArg[0];
+                int time = int.Parse(inputArg[2]);
+
+                if (!data.ContainsKey(name))
+                {
+                    data[name] = new SortedDictionary<string, int>();
+                }
+
+                if (!data[name].ContainsKey(IP))
+                {
+                    data[name][IP] = 0;
+                }
+
+                data[name][IP] += time;
+            }
+
+            foreach (var person in data)
+            {
+                Console.Write($"{person.Key}: ");
+
+                int totalTime = person.Value.Values.Sum();
+
+                Console.WriteLine($"{totalTime} [{string.Join(", ", person.Value.Keys)}]");
+            }
+        }
+    }
+}
