7dcb129ab98d478cc5706deba353ae1ae2b34a06
Stefan Angelov <ashmodean_@abv.bg>
2023-12-23 14:02:10 +0200

    upload task 19

---
+++ 55.HashSet and Dic/19.Population Counter/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace Population_Counter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, long>> data = new Dictionary<string, Dictionary<string, long>>();
+
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "report")
+            {
+                string[] inputARG = input.Split('|');
+
+                string country = inputARG[1];
+                string city = inputARG[0];
+                long population = long.Parse(inputARG[2]);
+
+                if (!data.ContainsKey(country))
+                {
+                    data[country] = new Dictionary<string, long>(); 
+                }
+
+                if (!data[country].ContainsKey(city))
+                {
+                    data[country][city] = 0;
+                }
+
+                data[country][city] = population;
+            }
+
+            foreach (var kvp in data.OrderByDescending(x =>x.Value.Values.Sum()))
+            {
+                string country = kvp.Key;
+                long population = kvp.Value.Values.Sum();
+
+                Console.WriteLine($"{country} (total population: {population})");
+
+                foreach (var kvp1 in kvp.Value.OrderByDescending(x =>x.Value))
+                {
+                    string city = kvp1.Key;
+                    long cityPoputation = kvp1.Value;
+
+                    Console.WriteLine($"=>{city}: {cityPoputation}");
+                }
+            }
+        }
+    }
+}
