a7be40fcd9ccd610821dec8b562ea3dd84d94528
Stefan Angelov <ashmodean_@abv.bg>
2023-12-25 21:51:28 +0200

    upload task  23

---
+++ 55.HashSet and Dic/23.Сръбско Unleashed 2/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Сръбско_Unleashed_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dictionary<string, List<Singer>> data = new Dictionary<string, List<Singer>>();
+
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string pattern = @"(?<name>[\w\s]+) @(?<location>[\w+\s]+) (?<price>\d+) (?<ticket>\d+)";
+
+                Regex regex = new Regex(pattern);
+
+                foreach (Match match in Regex.Matches(input, pattern))
+                {
+                    string location = match.Groups["location"].Value;
+                    string name = match.Groups["name"].Value;
+                    decimal income = decimal.Parse(match.Groups["price"].Value) * decimal.Parse(match.Groups["ticket"].Value);
+
+                    Singer current = new Singer(name);
+                    current.Income.Push(income);
+
+                    if (!data.ContainsKey(location))
+                    {
+                        data[location] = new List<Singer>();
+                    }
+
+                    if (!data[location].Contains(current))
+                    {
+                        data[location].Add(current);
+                    }
+                    else
+                    {
+                        Singer target = data[location].FirstOrDefault(x =>x.Name == name);
+                        target.Income.Push(income);
+                    }
+                }
+            }
+
+            foreach (var location in data)
+            {
+                Console.WriteLine(location.Key);
+
+                foreach (var singer in location.Value.OrderByDescending(x => x.Revenue))
+                {
+                    Console.WriteLine($"#  {singer.Name} -> {singer.Revenue}");
+                }
+            }
+
+           
+        }
+    }
+    class Singer
+    {
+        public string Name { get; set; }
+        public Stack<decimal> Income { get; set; }
+
+        public decimal Revenue
+        {
+            get
+            {
+                return Income.Sum();
+            }
+        }
+
+        public Singer(string name)
+        {
+            this.Name = name;
+            this.Income = new Stack<decimal>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Singer)
+            {
+                Singer other = (Singer)obj;
+
+                return this.Name == other.Name;
+            }
+
+            return false;
+        }
+
+    }
+}
