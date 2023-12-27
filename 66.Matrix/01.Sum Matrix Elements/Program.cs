dbe18068a4519065b21d7b4c335d721200a32091
Stefan Angelov <ashmodean_@abv.bg>
2023-12-27 20:28:00 +0200

    upload task 1

---
+++ 66.Matrix/01.Sum Matrix Elements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowCol = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int[][] matrix = new int[rowCol[0]][];
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = Console.ReadLine()
+                    .Split(", ")
+                    .Select(int.Parse)
+                    .ToArray();                    
+            }
+
+            Console.WriteLine(matrix.Length);
+            Console.WriteLine(matrix[0].Length);
+
+            Console.WriteLine(matrix.Sum(x =>x.Sum()));
+        }  
+    }
+}
