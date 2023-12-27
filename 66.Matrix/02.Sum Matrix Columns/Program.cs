815d1ab39e61ae2488d85218ed383fabe5561169
Stefan Angelov <ashmodean_@abv.bg>
2023-12-27 21:33:50 +0200

    upload task 2

---
+++ 66.Matrix/02.Sum Matrix Columns/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Sum_Matrix_Columns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowCol = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int[][] matrix = new int[rowCol[0]][];
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+            }            
+
+            for (int i = 0; i < matrix[0].Length; i++)
+            {
+                int sum = 0;
+
+                for (int j = 0; j < matrix.Length; j++)
+                {
+                    sum += matrix[j][i];
+                }
+
+                Console.WriteLine(sum);               
+            }
+        }
+    }
+}
