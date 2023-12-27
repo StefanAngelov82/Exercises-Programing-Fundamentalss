976a3b92c89ba5671215998275c318b4585f3d11
Stefan Angelov <ashmodean_@abv.bg>
2023-12-27 22:01:29 +0200

    upload task 3

---
+++ 66.Matrix/03.Primary Diagonal/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Primary_Diagonal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int row =int.Parse(Console.ReadLine());
+
+            int[][] matrix = new int[row][];
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+
+            int sum = 0;
+
+            for (int i = 0; i < row; i++)
+            {          
+                sum += matrix[i][i];                
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
