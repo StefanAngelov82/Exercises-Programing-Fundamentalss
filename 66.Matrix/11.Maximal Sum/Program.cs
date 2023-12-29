bcdfee223858944d037f3c3f6dc2481b294f8147
Stefan Angelov <ashmodean_@abv.bg>
2023-12-29 16:58:44 +0200

    upload task 11

---
+++ 66.Matrix/11.Maximal Sum/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+
+namespace Maximal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowCol = Console.ReadLine()
+               .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+
+
+            int[][] matrix = new int[rowCol[0]][];
+
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = Console.ReadLine()
+                    .Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+
+            int maxSum = int.MinValue;
+
+            // Console.WriteLine($"Enter rows:");
+
+            int row = 3;//int.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"Enter cols");
+
+            int col = 3; //int.Parse(Console.ReadLine());
+
+            int startRow = 0;
+            int startCol = 0;
+
+            for (int i = 0; i <= matrix.Length - row; i++)
+            {
+                for (int j = 0; j <= matrix[i].Length - col; j++)
+                {
+                    int sum = 0;
+
+                    for (int k = i; k < i + row; k++)
+                    {
+                        for (int i1 = j; i1 < j + col; i1++)
+                        {
+                            sum += matrix[k][i1];
+                        }
+                    }
+
+                    if (sum > maxSum)
+                    {
+                        maxSum = sum;
+                        startRow = i;
+                        startCol = j;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Sum = {maxSum}");
+
+            for (int i = startRow; i < startRow + row; i++)
+            {
+                for (int j = startCol; j < startCol + col; j++)
+                {
+                    Console.Write($"{matrix[i][j]} ");
+                }
+
+                Console.WriteLine();
+            }            
+        }
+    }
+}
