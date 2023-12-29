7e9749be59a035f2cbda883905ee191d5e791ef0
Stefan Angelov <ashmodean_@abv.bg>
2023-12-29 16:49:34 +0200

    upload task 10

---
+++ 66.Matrix/10.Matrix of Palindromes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Matrix_of_Palindromes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] rowCol = Console.ReadLine()
+                .Split(new string[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            string[][] matrix = new string[rowCol[0]][];
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                matrix[i] = new string[rowCol[1]];
+
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                   char firsSymbol = (char)(97 + i);
+                   char lastSymbol = (char)(97 + i);
+                   char middleSymbol = (char)(97 + i + j);                
+
+                    matrix[i][j] = "" + firsSymbol + middleSymbol + lastSymbol;
+                }                
+            }
+
+            foreach (var array in matrix)
+            {
+                Console.WriteLine(string.Join(" ",array));
+            }
+        }
+    }
+}
