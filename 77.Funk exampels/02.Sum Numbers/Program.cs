f0dd503566ce7c648abe0ed00588c946f729ad9c
Stefan Angelov <ashmodean_@abv.bg>
2024-01-02 22:47:27 +0200

    upload task 2

---
+++ 77.Funk exampels/02.Sum Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int[] data = input.Split(", ").Select(Parse1).ToArray();
+            Console.WriteLine(data.Count());
+            Console.WriteLine(data.Sum());
+
+
+        }
+
+        static int Parse1(string str) => int.Parse(str);
+        
+    }
+}
