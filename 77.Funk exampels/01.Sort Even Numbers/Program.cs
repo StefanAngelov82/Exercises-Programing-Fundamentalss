c5120e6ac2391a2056de8f0c419afe9b02f646a5
Stefan Angelov <ashmodean_@abv.bg>
2024-01-02 21:21:51 +0200

    upload task 1

---
+++ 77.Funk exampels/01.Sort Even Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Sort_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> data  = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToList();
+
+             data = data.Where(x => x % 2 == 0)
+                .OrderBy(x => x)
+                .ToList();
+
+            Console.WriteLine(string.Join(", ", data));
+
+
+        }
+    }
+}
