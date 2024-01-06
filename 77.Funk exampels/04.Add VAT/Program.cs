8077ef7133f1da4acdc1a78d6de3068b995a68e3
Stefan Angelov <ashmodean_@abv.bg>
2024-01-06 14:53:33 +0200

    upload task 04

---
+++ 77.Funk exampels/04.Add VAT/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split(", ")
+                .Select(double.Parse)
+                .Select(x => x *= 1.2)
+                .ToList()
+                .ForEach(x => Console.WriteLine($"{x:F2}"));
+        }
+    }
+}
