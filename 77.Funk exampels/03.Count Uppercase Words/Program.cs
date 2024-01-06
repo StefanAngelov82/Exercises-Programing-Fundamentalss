61d3732d7e9d4611efb5ce22f77b311e2c569c4b
Stefan Angelov <ashmodean_@abv.bg>
2024-01-06 14:21:53 +0200

    upload task 03

---
+++ 77.Funk exampels/03.Count Uppercase Words/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+
+            Func<string, bool> IsCapitalLetter = (string x) => (x.Length > 0) && (char.IsUpper(x[0]));
+
+            Console.ReadLine()
+                .Split()
+                .Where(x => IsCapitalLetter(x))
+                .ToList()
+                .ForEach(x => Console.WriteLine(x));
+        }        
+    }
+}
