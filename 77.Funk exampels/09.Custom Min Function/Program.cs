a3039c53a5a79323bf40e070243448fe53e9483b
Stefan Angelov <ashmodean_@abv.bg>
2024-01-07 17:07:19 +0200

    09 task upload

---
+++ 77.Funk exampels/09.Custom Min Function/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Func<int[], int> MinValue = number => number.Min();
+                
+            Console.WriteLine(MinValue(numbers));
+        }
+    }
+}
