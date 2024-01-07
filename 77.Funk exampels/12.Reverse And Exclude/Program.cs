fdadc26624d736049fcb1b169936630fd5ed0aca
Stefan Angelov <ashmodean_@abv.bg>
2024-01-07 21:16:47 +0200

    upload task 12

---
+++ 77.Funk exampels/12.Reverse And Exclude/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Reverse_And_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int dev = int.Parse(Console.ReadLine());
+
+            Action<int[]> reverse = (x) => Array.Reverse(x);
+
+            reverse(numbers);
+
+            Predicate<int> filter = (x) => x % dev != 0;
+
+            numbers.Where(x => filter(x))
+                .ToList()
+                .ForEach(x => Console.Write($"{x} "));
+
+        }
+    }
+}
