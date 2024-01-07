96f383a09d81dcd03f1433b5048d5ca73c3af9ba
Stefan Angelov <ashmodean_@abv.bg>
2024-01-07 16:48:51 +0200

    upload task 8

---
+++ 77.Funk exampels/08.Knights of Honor/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+
+            Func<string, string> printName = name => "Sir " + name;
+
+            Array.ForEach(names, name => Console.WriteLine(printName(name)));
+        }
+    }
+}
