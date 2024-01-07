5b6fe91638418ad3b7a02e5e1a90e82ee2f3f6e6
Stefan Angelov <ashmodean_@abv.bg>
2024-01-07 17:47:10 +0200

    upload task 10

---
+++ 77.Funk exampels/10.Find Evens or Odds/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Find_Evens_or_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] range = Console.ReadLine()
+                .Split()
+                .Select(long.Parse)
+                .ToArray();
+
+            List<long> numbers = new List<long>();
+
+            for (long i = range[0]; i <= range[1]; i++)
+            {
+                numbers.Add(i);
+            }
+
+            string Input = Console.ReadLine();
+
+            Predicate<long> isRequired =
+                (x) =>
+                {
+                    if (Input == "even") return Math.Abs(x) % 2 == 0;
+                    else                 return Math.Abs(x) % 2 == 1;                    
+                };
+
+            numbers.Where(x => isRequired(x)).ToList().ForEach(x => Console.Write($"{x} "));
+        }
+    }
+}
