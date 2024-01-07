96e7d4665bdccbcbe4fac9d6ffe74cd1c1e75340
Stefan Angelov <ashmodean_@abv.bg>
2024-01-07 16:10:21 +0200

    upload task 7

---
+++ 77.Funk exampels/07.Action Point/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Action_Point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            Action<string> print = x => Console.WriteLine(x);
+
+            names.ForEach(print);
+        }
+    }
+}
