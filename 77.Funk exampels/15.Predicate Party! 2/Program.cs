d62ea8dd1a570719aa3d1e554d817455869b2a6a
Stefan Angelov <ashmodean_@abv.bg>
2024-01-09 21:09:57 +0200

    upload task 15

---
+++ 77.Funk exampels/15.Predicate Party! 2/Program.cs
@@ -0,0 +1,71 @@
+﻿
+
+namespace Predicate_Party_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> gests = Console.ReadLine()
+                .Split()
+                .ToList();
+
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "Party!")
+            {
+                string[] inputArg = input.Split();
+
+                string criteria = inputArg[1];
+                string nameFeature = inputArg[2];
+
+               Predicate<string> check = NameProcessing(criteria, nameFeature);
+
+                switch (inputArg[0])
+                {
+                    case "Remove":
+
+                        gests.RemoveAll(check);
+
+                        break;
+                    case "Double":                        
+
+                        for (int i = 0; i < gests.Count; i++)
+                        {
+                            if (check(gests[i]))
+                            {
+                                gests.Insert(i, gests[i]);
+                                i++;
+                            }
+                        }
+                        break;
+
+                }
+            }
+
+            if (gests.Count != 0)
+                Console.WriteLine($"{string.Join(", ", gests)} are going to the party!");
+
+            else
+                Console.WriteLine("Nobody is going to the party!");
+
+            
+        }
+
+        static Predicate<string> NameProcessing(string criteria, string nameFeature)
+        {
+            if (criteria == "StartsWith")
+            {
+                return x => x.IndexOf(nameFeature) == 0;
+            }
+            else if (criteria == "EndsWith")
+            {
+                return x => x.LastIndexOf(nameFeature) == x.Length - nameFeature.Length;
+            }
+            else
+            {
+                return x => x.Length == int.Parse(nameFeature);
+            }
+        }
+    }
+}
