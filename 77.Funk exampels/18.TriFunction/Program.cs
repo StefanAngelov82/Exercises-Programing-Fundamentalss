c232f5ffffcadb38432e755fb979674361bffe02
Stefan Angelov <ashmodean_@abv.bg>
2024-01-10 22:12:42 +0200

    upload task 18

---
+++ 77.Funk exampels/18.TriFunction/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TriFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split();
+
+
+            Func<string, int, bool> check =
+                (x, y) =>
+                {
+                    int count = 0;
+
+                    for (int i = 0; i < x.Length; i++)
+                    {
+                        count += x[i];
+                    }
+
+                    return count >= y;
+                };
+
+            foreach (string name in names)
+            {
+                if (check(name, N))
+                {
+                    Console.WriteLine(name);
+                    break;
+                }
+            };                 
+        }
+    }
+}
