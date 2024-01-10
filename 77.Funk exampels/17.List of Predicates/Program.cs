acac489285db08748374a111c7d84025c1b36ee1
Stefan Angelov <ashmodean_@abv.bg>
2024-01-10 21:39:58 +0200

    upload task 17

---
+++ 77.Funk exampels/17.List of Predicates/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace List_of_Predicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            List<int> list = new List<int>();
+
+            for (int i = 1; i <= N; i++)
+            {
+                list.Add(i);
+            }
+
+            int[] dividers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Predicate<int> check =
+                (x) =>
+                {
+                    bool isDiv = true;
+
+                    for (int i = 0; i < dividers.Length; i++)
+                    {
+                        if (x % dividers[i] != 0)
+                        {
+                            isDiv = false;
+                            break;
+                        }
+                    }
+
+                    return isDiv;
+                };
+
+            list.FindAll(check).ToList().ForEach(x => Console.Write($"{x} "));
+
+        }
+
+    }
+}
