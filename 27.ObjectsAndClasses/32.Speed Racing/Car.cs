1f7e9e390c7a6c5d3e507ed70aa6bebc0497c6b3
Stefan Angelov <ashmodean_@abv.bg>
2024-01-14 16:58:37 +0200

    upload task 32

---
+++ 27.ObjectsAndClasses/32.Speed Racing/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+
+		private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKilometer;
+        private double travelledDistance;
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+        {
+            this.Model = model;
+            this.fuelAmount = fuelAmount;
+            this.fuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            this.travelledDistance = 0;
+        }
+
+
+
+        public string Model
+		{
+			get { return model; }
+			set { model = value; }
+		}		
+
+		public double FuelAmount
+        {
+			get { return fuelAmount; }
+			set { fuelAmount = value; }
+		}	
+
+		public double FuelConsumptionPerKilometer
+        {
+			get { return fuelConsumptionPerKilometer; }
+			set { fuelConsumptionPerKilometer = value; }
+		}      
+
+        public double TravelledDistance
+		{
+			get { return travelledDistance; }
+			set { travelledDistance = value; }
+		}
+
+
+		public void CarMove(double amountOfKm)
+		{
+			double consumedFuel = amountOfKm * this.FuelConsumptionPerKilometer;
+
+            if (this.FuelAmount >= consumedFuel) 
+            {
+                this.FuelAmount -= consumedFuel;
+                this.TravelledDistance += amountOfKm;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+	}
+}
---
+++ 27.ObjectsAndClasses/32.Speed Racing/StartUp.cs
@@ -0,0 +1,56 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<Car> data = new List<Car>();
+
+            EnteringData(data);
+            Traveling(data);
+
+            foreach (var car in data)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:F2} {car.TravelledDistance}");
+            }
+        }
+
+        private static void Traveling(List<Car> data)
+        {
+            string Command = String.Empty;
+
+            while ((Command = Console.ReadLine()) != "End")
+            {
+                string[] CommandArg = Command.Split();
+
+                string carModel = CommandArg[1];
+                double amountOfKm = double.Parse(CommandArg[2]);
+
+                Car current = data.FirstOrDefault(x => x.Model == carModel);
+
+                if (current != null)
+                {
+                    current.CarMove(amountOfKm);
+                }
+            }
+        }
+
+        private static void EnteringData(List<Car> data)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string model = input[0];
+                double fuelAmount = double.Parse(input[1]);
+                double fuelConsumptionFor1km = double.Parse(input[2]);
+
+                Car current = new Car(model, fuelAmount, fuelConsumptionFor1km);
+
+                data.Add(current);
+            }
+        }
+    }
+}
