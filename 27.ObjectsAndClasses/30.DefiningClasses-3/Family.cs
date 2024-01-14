0fa51223af0d19b7a8b3ec0dd2f819ca40c27473
Stefan Angelov <ashmodean_@abv.bg>
2024-01-14 12:43:15 +0200

    upload task 30

---
+++ 27.ObjectsAndClasses/30.DefiningClasses-3/Family.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public List<Person> Collection { get; set; }
+
+        public Family()
+        {
+            this.Collection = new List<Person>();
+        }
+
+        public void AddMember(Person p)
+        {
+            this.Collection.Add(p);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.Collection.OrderByDescending(x => x.Age).First();
+        }
+    }
+}
---
+++ 27.ObjectsAndClasses/30.DefiningClasses-3/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            Family Petrovi = new Family();
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person current = new Person(name, age);
+
+                Petrovi.AddMember(current);                
+            }
+
+            Person oldest = Petrovi.GetOldestMember();
+
+            Console.WriteLine($"{oldest.Name} {oldest.Age}");
+        }
+    }
+}
