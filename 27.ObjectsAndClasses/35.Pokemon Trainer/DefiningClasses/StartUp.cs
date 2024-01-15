e8c25ae223f84e567d6ec48448b9353344f1becf
Stefan Angelov <ashmodean_@abv.bg>
2024-01-15 23:12:58 +0200

    upload task 35

---
+++ 27.ObjectsAndClasses/35.Pokemon Trainer/DefiningClasses/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Channels;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<Trainers> data = new List<Trainers>();
+
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "Tournament")
+            {
+                string[] inputArg = input.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+                string trainerName = inputArg[0];
+                string pokemonName = inputArg[1];
+                string pokemonElement = inputArg[2];
+                int pokemonHealth = int.Parse(inputArg[3]);
+
+                Pokemon currentPokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+
+                Trainers currentTrainer = data.FirstOrDefault(x => x.Name == trainerName);
+
+                if (currentTrainer != null)
+                {
+                    currentTrainer.Pokemons.Add(currentPokemon);
+                }
+                else
+                {
+                    currentTrainer = new Trainers(trainerName);
+                    currentTrainer.Pokemons.Add(currentPokemon);
+                    data.Add(currentTrainer);
+                }               
+            }
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                foreach (var trainer in data)
+                {
+                    if (trainer.Pokemons.Any(x => x.Element == input))
+                    {
+                        trainer.Badges += 1;
+                    }
+                    else
+                    {
+                        trainer.Pokemons.ForEach(x => x.Health -= 10);
+                        trainer.Pokemons.RemoveAll(x => x.Health <= 0);
+                    }
+                }
+            }
+
+            data.OrderByDescending(x => x.Badges).ToList().ForEach(x => Console.WriteLine($"{x.Name} {x.Badges} {x.Pokemons.Count}"));            
+        }
+    }
+}
---
+++ 27.ObjectsAndClasses/35.Pokemon Trainer/DefiningClasses/Trainers.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Trainers
+    {
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List<Pokemon> Pokemons { get; set; }
+
+
+        public Trainers(string name)
+        {
+            this.Name = name;
+            this.Badges = 0;
+            this.Pokemons = new List<Pokemon>();
+        }
+    }
+}
