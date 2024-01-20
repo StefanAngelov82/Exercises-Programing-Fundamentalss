5961b9000c7243a82585e1ea00d1c5246f9509f7
Stefan Angelov <ashmodean_@abv.bg>
2024-01-20 15:39:19 +0200

    upload task 2

---
+++ 88.Generics/2.GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericArrayCreator
+{
+    public class ArrayCreator
+    {     
+        public static T[] Create<T>(int length, T item)
+        {
+            T[] array  = new T[length];
+
+            return array =  array.Select(x => x = item).ToArray();            
+        }
+    }
+}
---
+++ 88.Generics/2.GenericArrayCreator/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            
+            double[] integers = ArrayCreator.Create(10, 33.01);
+
+            for (int i = 0; i < integers.Length; i++)
+            {
+                Console.WriteLine(integers[i]);
+            }
+
+        }
+    }
+}
