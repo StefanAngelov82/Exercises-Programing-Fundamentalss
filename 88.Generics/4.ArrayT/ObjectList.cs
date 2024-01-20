6d7ca6d24b7eb68ac1a4bd1f7a9c37679c246b75
Stefan Angelov <ashmodean_@abv.bg>
2024-01-20 17:30:13 +0200

    upload task 4

---
+++ 88.Generics/4.ArrayT/ObjectList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayT
+{
+    public class ObjectList<T>
+    {
+        private int index = 0;
+        public T[] Data { get; set; }
+        
+
+        public ObjectList(int capacity)
+        {
+            this.Data = new T[capacity];
+        }
+
+        public void Add(T item)
+        {
+            this.Data[index] = item;
+            index++;
+        }
+
+        public T GetData(int index)
+        {
+            return this.Data[index];
+        }
+    }
+}
---
+++ 88.Generics/4.ArrayT/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace ArrayT
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            ObjectList<int> gg = new ObjectList<int>(5);
+
+            gg.Add(199);
+            gg.Add(166);
+            Console.WriteLine(gg.GetData(0));
+        }
+    }
+}
