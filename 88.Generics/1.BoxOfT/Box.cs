4192f4703948b85f1201eb96e4923e2f0afa2360
Stefan Angelov <ashmodean_@abv.bg>
2024-01-20 11:36:17 +0200

    upload task 1

---
+++ 88.Generics/1.BoxOfT/Box.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public Stack<T> Items { get; set; }
+        public int Count { get => Items.Count;}
+
+        public Box()
+        {
+            this.Items = new Stack<T>();
+        }
+
+        public void Add(T element)
+        {
+            Items.Push(element);
+        }
+
+        public T Remove()
+        {
+            return Items.Pop();
+        }
+    }
+}
