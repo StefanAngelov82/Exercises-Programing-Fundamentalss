d30df52b5875f36b489a1d0b257f1b81472ea7fc
Stefan Angelov <ashmodean_@abv.bg>
2024-01-20 16:13:26 +0200

    upload task 3

---
+++ 88.Generics/3.GenericScale/EqualityScale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericScale
+{
+    public class EqualityScale<T> //where T : IEnumerable<T>
+    {
+        public EqualityScale(T left, T right)
+        {
+            this.Left = left;
+            this.Right = right;
+        }
+
+        public T Left { get; set; }
+        public T Right { get; set; }
+
+        public bool AreEqual()
+        {
+            return Left.Equals(Right);
+        }
+    }
+}
---
+++ 88.Generics/3.GenericScale/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> current = new EqualityScale<int>(5, 6);
+
+            Console.WriteLine(current.AreEqual());
+        }
+    }
+}
