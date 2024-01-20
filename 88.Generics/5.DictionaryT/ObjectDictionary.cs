36d5fb0ef1b9fc10e24178ac32df54fd6f204c53
Stefan Angelov <ashmodean_@abv.bg>
2024-01-20 17:53:02 +0200

    upload task 5

---
+++ 88.Generics/5.DictionaryT/ObjectDictionary.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryT
+{
+    public class ObjectDictionary<TKeys, TValues>
+    {
+        private int index = 0;
+        private TKeys[] key;
+        private TValues[] values;
+
+        public ObjectDictionary(int capacity)
+        {
+            this.key = new TKeys[capacity];
+            this.values = new TValues[capacity];
+        }
+
+
+        public void AddData(TKeys key, TValues values)
+        {
+            this.key[index] = key;
+            this.values[index] = values;    
+            index++;
+        }
+
+        public TValues GetData(TKeys key)
+        {
+            var index = Array.IndexOf(this.key, key);
+
+            return this.values[index];
+        }
+    }
+}
---
+++ 88.Generics/5.DictionaryT/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace DictionaryT
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ObjectDictionary<string, int> dic = new ObjectDictionary<string, int>(5);
+
+            dic.AddData("Pesho", 5);
+            dic.AddData("Mimi", 6);
+
+            Console.WriteLine(dic.GetData("Mimi"));
+        }
+    }
+}
