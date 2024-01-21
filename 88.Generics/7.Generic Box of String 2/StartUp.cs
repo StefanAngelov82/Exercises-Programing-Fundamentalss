374829241efb3c4eab414ef02d8b75241ec0dccb
Stefan Angelov <ashmodean_@abv.bg>
2024-01-21 12:29:12 +0200

    upload task 7

---
+++ 88.Generics/7.Generic Box of String 2/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using GenericBoxofString;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                int data = int.Parse( Console.ReadLine());
+
+                Box<int> box = new Box<int>(data);
+
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
