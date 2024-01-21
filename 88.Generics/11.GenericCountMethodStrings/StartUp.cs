482af60c9bd73a349ff665d0c410479f87d9f95a
Stefan Angelov <ashmodean_@abv.bg>
2024-01-21 18:30:28 +0200

    upload task 11

---
+++ 88.Generics/11.GenericCountMethodStrings/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace GenericCountMethodStrings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<double> list = new List<double>();
+
+            double N = double.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                list.Add(double.Parse(Console.ReadLine()));
+            }
+
+            double pattern = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Compare(list, pattern));
+        }
+        static int Compare<T>(List<T> list, T pattern)  where T : IComparable 
+        {
+            int counter = 0;
+            
+            foreach (var item in list)
+            {
+                if (item.CompareTo(pattern) > 0)
+                {
+                    counter++;
+                }
+            }           
+
+            return counter;
+        }
+    }
+}
