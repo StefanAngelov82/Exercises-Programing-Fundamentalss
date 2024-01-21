7212f0e56ecddd959e071733eb69dbc51926c5a9
Stefan Angelov <ashmodean_@abv.bg>
2024-01-21 12:15:26 +0200

    upload task 6

---
+++ 88.Generics/6.Generic Box of String/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericBoxofString
+{
+    public class Box<T>
+    {
+
+        public T Data { get; set; }
+
+        public Box(T data)
+        {
+            this.Data = data;
+        }
+
+        public override string ToString()
+        {
+            return $"{ typeof(T).FullName}: {Data.ToString()} ";
+        }
+    }
+}
---
+++ 88.Generics/6.Generic Box of String/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using GenericBoxofString;
+
+namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string data =  Console.ReadLine();
+
+                Box<string> box = new Box<string>(data);
+
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
