679ce120bc70e823ec338d57b9f1878cc4455f65
Stefan Angelov <ashmodean_@abv.bg>
2024-01-21 16:15:41 +0200

    upload task 8

---
+++ 88.Generics/8.GenericSwapMethodStrings/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace GenericSwapMethodStrings
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<string> list = new List<string>();
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                string element = Console.ReadLine();
+                list.Add(element);
+            }
+
+            string command = Console.ReadLine();
+
+            int index1 = int.Parse(command.Split()[0]);
+            int index2 = int.Parse(command.Split()[1]);
+
+            Swap(list, index1, index2);
+
+            foreach (var element in list)
+            {
+                Console.WriteLine($"{element.GetType()}: {element}");
+            }                        
+        }
+
+        static void Swap<T>(List<T> data, int index1, int index2)
+        {
+            T temp = data[index1];
+            data[index1] = data[index2];
+            data[index2] = temp;
+        }
+    }
+}
