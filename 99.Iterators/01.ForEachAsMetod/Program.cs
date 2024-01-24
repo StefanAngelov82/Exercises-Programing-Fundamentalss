393e88299167fbb27602d9b095f8f2a1dece0497
Stefan Angelov <ashmodean_@abv.bg>
2024-01-24 20:31:47 +0200

    upload task 1

---
+++ 99.Iterators/01.ForEachAsMetod/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ForEachAsMetod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3};
+            Action<int> callBack = x => Console.WriteLine(x);
+
+           
+            ForEach(callBack, list);
+
+            
+
+
+            void ForEach<T>(Action<T> callBack, IEnumerable<T> collection)
+            {
+                IEnumerator<T> enumerator = collection.GetEnumerator();
+
+                while (enumerator.MoveNext())
+                {
+                    callBack(enumerator.Current);
+                }
+            }
+        }
+
+       
+    }
+}
