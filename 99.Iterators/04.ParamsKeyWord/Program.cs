c7476edde0880b9ce4e9d9a8cf729d63c52062d9
Stefan Angelov <ashmodean_@abv.bg>
2024-01-25 22:32:50 +0200

    upload task 4

---
+++ 99.Iterators/04.ParamsKeyWord/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ParamsKeyWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNames("Pesho", "Gosho", "Tosho");
+        }
+
+        static void PrintNames(params string[] names)
+        {
+            Console.WriteLine(String.Join(", ", names));
+        } 
+    }
+}
