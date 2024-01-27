ce1e0162cd87aaf004fad43a2b8e044db5d9e7b8
Stefan Angelov <ashmodean_@abv.bg>
2024-01-27 16:16:08 +0200

    upload task 10

---
+++ 99.Iterators/10.CompareStudents/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CompareStudents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student st1 = new Student( 3.76, "Pesho");
+            Student st2 = new Student( 3.70, "Asan");
+
+
+            Console.WriteLine(st1.CompareTo(st2) == 1 ? "Pesho is the best" : "Asan is the Best");
+        }
+    }
+} 
---
+++ 99.Iterators/10.CompareStudents/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareStudents
+{
+    internal class Student : IComparable<Student>, IComparable<int>
+    {
+        public Student(double averageScore, string name)
+        {
+            this.averageScore = averageScore;
+            this.Name = name;
+        }
+
+        public double averageScore { get; set; }
+        public string Name { get; set; }
+
+        public int CompareTo(Student? other)
+        {
+            if (this.averageScore < other.averageScore)
+            {
+                return -1;
+            }
+            else if (this.averageScore == other.averageScore)
+            {
+                return this.Name.CompareTo(other.Name);
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        public int CompareTo(int score)
+        {
+            if (this.averageScore < score)
+            {
+                return -1;
+            }
+            else if (this.averageScore == score)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
