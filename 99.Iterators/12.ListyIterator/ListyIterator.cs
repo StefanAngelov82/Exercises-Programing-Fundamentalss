05fe1d806daa9773debab3b7c167d784f9ec0afc
Stefan Angelov <ashmodean_@abv.bg>
2024-01-27 23:59:20 +0200

    upload  task 12

---
+++ 99.Iterators/12.ListyIterator/ListyIterator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> values;
+        private int index = 0;
+
+        public ListyIterator(List<T> values)
+        {
+            this.values = values;
+        }
+
+        public bool Move()
+        {
+            if (index < values.Count - 1)
+            {
+                index++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return index + 1 < values.Count;
+        }
+
+        public void Print()
+        {
+            if (values.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");                
+            }
+            else
+            {
+                Console.WriteLine(values[index]);
+            }
+        }
+
+    }
+}
