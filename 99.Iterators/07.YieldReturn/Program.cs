54740e6cad633f8f69fa123e83e148c6e604d2be
Stefan Angelov <ashmodean_@abv.bg>
2024-01-27 10:54:37 +0200

    upload task 7

---
+++ 99.Iterators/07.YieldReturn/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+namespace YieldReturn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyEnunm list = new MyEnunm();
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+
+        
+
+    }
+
+    class MyEnunm : IEnumerable<int>
+    {
+        public IEnumerator<int> GetEnumerator() => GenerateNumbers();       
+
+        IEnumerator IEnumerable.GetEnumerator() => GenerateNumbers();
+        
+
+        public IEnumerator<int> GenerateNumbers()
+        {
+            yield return 1;
+            yield return 5;
+            yield return 9;
+            yield return 10;
+        }
+    }
+}
