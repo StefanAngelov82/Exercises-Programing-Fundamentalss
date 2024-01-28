da04abe152e9213a23a096d9a39234b5c1fa9729
Stefan Angelov <ashmodean_@abv.bg>
2024-01-28 13:41:52 +0200

    upload task 14

---
+++ 99.Iterators/14.Stack/CustomStack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {        
+        private T[] data = new T[0];        
+
+        public void Push(T[] data)
+        {
+            int addIndex = data.Length;
+            int baseIndex = this.data.Length;
+
+            int newIndex = addIndex + baseIndex;
+
+            T[] newData = new T[newIndex];
+
+            newData = this.data.Concat(data).ToArray();           
+            this.data = newData;
+        }
+
+        public void Pop()
+        {
+            if (this.data.Length == 0)
+            {
+                Console.WriteLine("No elements");
+            }
+            else
+            {
+
+                T[] newData = new T[this.data.Length - 1];
+                newData = this.data[0..(this.data.Length - 1)];
+                this.data = newData;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = data.Length - 1; j >= 0; j--)
+                {
+                    yield return data[j];
+                }
+            }
+            
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        
+    }
+}
---
+++ 99.Iterators/14.Stack/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace Stack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<string> CS = new CustomStack<string>();
+
+            string input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string command = input.Split(new string[] {" ", ", " }, StringSplitOptions.RemoveEmptyEntries)[0];
+                string[] data = input.Split(new string[] { " ", ", " }, StringSplitOptions.RemoveEmptyEntries)[1..];
+                    
+                switch (command)
+                {
+                    case "Push":
+                        CS.Push(data);
+                        break;
+
+                    case "Pop":
+                        CS.Pop();
+                        break;
+                }
+            }
+
+            foreach (var i in CS)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
