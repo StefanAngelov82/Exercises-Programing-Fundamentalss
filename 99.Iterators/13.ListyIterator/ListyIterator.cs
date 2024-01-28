9cb13888c2cb1eac9a1f9f18b1cf000a5266a138
Stefan Angelov <ashmodean_@abv.bg>
2024-01-28 11:22:38 +0200

    upload task 13

---
+++ 99.Iterators/13.ListyIterator/ListyIterator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T> : IEnumerable<T>     
+        {
+        private List<T> values;
+        private int index = 0;
+
+        public ListyIterator(List<T> values)
+        {
+            this.values = values;
+        }
+
+        public bool Move()
+        {
+            if (index < values.Count - 1)
+            {
+                index++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            return index + 1 < values.Count;
+        }
+
+        public void Print()
+        {
+            if (values.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");                
+            }
+            else
+            {
+                Console.WriteLine(values[index]);
+            }
+        }      
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in values)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ 99.Iterators/13.ListyIterator/StartUp.cs
@@ -0,0 +1,52 @@
+﻿namespace ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {            
+            List<string> values = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Skip(1)
+                .ToList();          
+
+            ListyIterator<string> LS = new ListyIterator<string>(values);
+
+            string command = String.Empty;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                switch (command)
+                {
+                    case "Move":
+                        Console.WriteLine(LS.Move());
+                        break;
+
+                    case "Print":
+                        try
+                        {
+                            LS.Print();
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+
+                    case "HasNext":
+                        Console.WriteLine(LS.HasNext());
+                        break;
+                         
+                    case "PrintAll":
+                        foreach (var item in LS)
+                        {
+                            Console.Write($"{item} ");
+                        }
+
+                        Console.WriteLine();
+                        break;
+
+                }
+            }
+        }
+    }
+}
