a4fdcc329872e1ea0e325914d95f439a322b9de0
Stefan Angelov <ashmodean_@abv.bg>
2024-01-29 23:20:37 +0200

    upload task 16

---
+++ 99.Iterators/16.EqualityLogic/Person.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqualityLogic
+{
+    public class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public int CompareTo(Person? other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+
+            if (result != 0)
+            {
+                return result;
+            }
+
+            return this.Age.CompareTo(other.Age);   
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Person)
+            {
+                Person other = (Person)obj;
+
+                return other.Name== this.Name &&
+                       other.Age ==this.Age;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = this.Name.GetHashCode() + this.Age.GetHashCode();
+
+            return hashCode;
+        }
+
+    }
+
+    
+}
---
+++ 99.Iterators/16.EqualityLogic/StarUp.cs
@@ -0,0 +1,32 @@
+﻿namespace EqualityLogic
+{
+    public class StarUp
+    {
+        static void Main(string[] args)
+        {
+            HashSet <Person> set = new HashSet <Person> ();
+            SortedSet<Person>sortetdSet = new SortedSet <Person> ();
+
+            int N = int.Parse(Console.ReadLine());
+
+
+            for (int i = 0; i < N; i++)
+            {
+                string[] inputArg = Console.ReadLine()
+                    .Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+                string name = inputArg[0];
+                int age = int.Parse(inputArg[1]);
+
+                Person current = new Person (name, age);
+
+                set.Add (current);
+                sortetdSet.Add (current);
+
+            }
+
+            Console.WriteLine(set.Count);
+            Console.WriteLine(sortetdSet.Count);
+        }
+    }
+}
