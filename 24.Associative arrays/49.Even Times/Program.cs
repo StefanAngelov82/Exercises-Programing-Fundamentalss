d52fb0c2340d0e0c37f0b6bb8244e8a8b78598de
Stefan Angelov <ashmodean_@abv.bg>
2024-01-30 21:44:42 +0200

    upload task 49

---
+++ 24.Associative arrays/49.Even Times/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> data = new Dictionary<int, int>();
+
+            int N  = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (!data.ContainsKey(number))
+                {
+                    data[number] = 0;
+                }
+
+                data[number]++;
+            }
+
+            Console.WriteLine((data.FirstOrDefault(x=>x.Value % 2 == 0)).Key);
+        }
+    }
+}
