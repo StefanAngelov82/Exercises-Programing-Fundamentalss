85c5c1313362145d78fbba649292e92715391568
Stefan Angelov <ashmodean_@abv.bg>
2024-01-30 22:08:03 +0200

    upload task 50

---
+++ 24.Associative arrays/50.Count Symbols/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<char, int> data = new SortedDictionary<char, int>();
+
+            string input = Console.ReadLine();
+
+            foreach (var symbol in input)
+            {
+                if (!data.ContainsKey(symbol))
+                {
+                    data[symbol] = 0;
+                }
+
+                data[symbol]++;
+            }
+
+            foreach (var kvp in data)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
