a684117320fa0ec504bf7d45a2343a0191bcdee5
Stefan Angelov <ashmodean_@abv.bg>
2024-02-02 20:24:55 +0200

    upload task 23

---
+++ 44.Staks and Queues/23.Maximum and Minimum Element/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Maximum_and_Minimum_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> data = new();
+
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                switch (input[0])
+                {
+                    case 1:
+                        data.Push(input[1]);
+                        break;
+
+                    case 2:
+                        data.Pop();
+                        break;
+
+                    case 3:
+                        if(data.Any())
+                            Console.WriteLine(data.Max());
+                        break;
+
+                    case 4:
+                        if(data.Any())
+                            Console.WriteLine(data.Min());
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", data));
+        }
+    }
+}
