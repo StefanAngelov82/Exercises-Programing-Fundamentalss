827fde6d192402cc1f587d1c709cfc1751132ee1
Stefan Angelov <ashmodean_@abv.bg>
2024-02-02 22:52:21 +0200

    upload task 25

---
+++ 44.Staks and Queues/25.Fashion Boutique/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> box = new(
+                Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse));
+
+            int capacity = int.Parse(Console.ReadLine());
+            int currentCap = capacity;
+            int count = 1;
+            
+            if (box.Count == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            } 
+
+            while (box.Any())
+            {
+                currentCap -= box.Peek();
+
+                if (currentCap < 0)
+                {
+                    currentCap =  capacity;
+                    count++;
+
+                    continue;
+                }
+
+                box.Pop();                
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
