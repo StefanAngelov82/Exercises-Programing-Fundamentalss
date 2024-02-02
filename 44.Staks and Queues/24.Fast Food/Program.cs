45758b1beef7d2154ab00ee18ec44abb46835943
Stefan Angelov <ashmodean_@abv.bg>
2024-02-02 21:27:39 +0200

    upload task 24

---
+++ 44.Staks and Queues/24.Fast Food/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodOrder = int.Parse(Console.ReadLine());
+
+            Queue<int> orders = new (Console.ReadLine().Split().Select(int.Parse));
+
+            if (orders.Any())
+                Console.WriteLine(orders.Max());
+
+            while (orders.Any())
+            {
+                foodOrder -= orders.Peek();
+
+                if(foodOrder < 0) break;
+
+                orders.Dequeue();
+            }
+
+            if (!orders.Any())            
+                Console.WriteLine("Orders complete");
+           
+            else           
+                Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+        }
+    }
+}
