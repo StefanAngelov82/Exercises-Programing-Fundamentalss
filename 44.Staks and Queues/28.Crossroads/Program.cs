14c488b549e32c74a2c27327f6e3a9b7e91ab201
Stefan Angelov <ashmodean_@abv.bg>
2024-02-03 22:24:28 +0200

    upload task 28

---
+++ 44.Staks and Queues/28.Crossroads/Program.cs
@@ -0,0 +1,54 @@
+﻿
+Queue<string> queue = new Queue<string>();
+
+int greenTime = int.Parse(Console.ReadLine()); 
+int freeTime = int.Parse(Console.ReadLine());
+int passedCars = 0;
+
+string input = String.Empty;
+
+
+while ((input = Console.ReadLine()) != "END")
+{
+    if (input != "green")
+    {
+        queue.Enqueue(input);
+    }
+    else
+    {
+        int currentGreenTime = greenTime;
+        int currentFreeTime = freeTime;
+
+        while (queue.Any() && currentGreenTime > 0)
+        {
+            if (queue.Peek().Length <= currentGreenTime)
+            {                
+                currentGreenTime -= queue.Dequeue().Length;
+                passedCars++;
+            }
+            else
+            {
+                int restPart = queue.Peek().Length - currentGreenTime;
+
+                if (restPart <= freeTime)
+                {
+                    queue.Dequeue();
+                    passedCars++;
+                    currentGreenTime = 0;
+                }
+                else
+                {
+                    int hitIndex = queue.Peek().Length - currentGreenTime - freeTime ;
+
+                    Console.WriteLine("A crash happened!");
+                    Console.WriteLine($"{queue.Peek()} was hit at {queue.Peek()[^hitIndex]}.");
+                    return;
+                }
+            }
+        }
+    }
+}
+
+
+Console.WriteLine("Everyone is safe.");
+Console.WriteLine($"{passedCars} total cars passed the crossroads.");
