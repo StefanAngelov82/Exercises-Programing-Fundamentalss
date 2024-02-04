b6ce9d88bd68a2c6f3ce821fe8615990ceb4f20b
Stefan Angelov <ashmodean_@abv.bg>
2024-02-04 21:43:44 +0200

    upload task 15

---
+++ 66.Matrix/15.Diagonal Difference2/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+
+int[][] matrix = new int[n][];  
+
+for (int i = 0; i < n; i++)
+{
+    matrix[i] = Console.ReadLine()
+        .Split()
+        .Select(int.Parse)
+        .ToArray();
+}
+
+int sum1 = 0;
+int sum2 = 0;
+
+for (int i = 0;i < matrix.Length; i++)
+{
+    sum1 += matrix[i][i];
+    sum2 += matrix[i][matrix[i].Length - 1 - i];
+}
+
+Console.WriteLine(Math.Abs(sum1 - sum2));
