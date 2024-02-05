9e9d7cb7c52d3434ec5d1483d9f69c62a0959d04
Stefan Angelov <ashmodean_@abv.bg>
2024-02-05 15:44:45 +0200

    upload task 18

---
+++ 66.Matrix/18.Snake Moves2/Program.cs
@@ -0,0 +1,37 @@
+﻿
+int[] rowCol = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+char[][] stairs = new char[rowCol[0]][];
+
+for (int i = 0; i < stairs.Length; i++)
+    stairs[i] = new char[rowCol[1]];
+
+Queue<char> snake1 = new Queue<char>(Console.ReadLine());
+
+for (int i = 0; i < stairs.Length; i++)
+{
+    if (i % 2 == 0)   
+        for (int j = 0; j < stairs[i].Length; j++)      
+            SnakeMove(stairs, snake1, i, j);      
+   
+    else    
+        for (int j = stairs[i].Length - 1; j >= 0; j--)        
+            SnakeMove(stairs, snake1, i, j);
+}
+
+
+for (int i = 0; i < stairs.Length; i++)
+{
+    for (int j = 0; j < stairs[i].Length; j++)   
+        Console.Write($"{stairs[i][j]}");
+    
+    Console.WriteLine();
+}
+static void SnakeMove(char[][] stairs, Queue<char> snake1, int i, int j)
+{
+    stairs[i][j] = snake1.Peek();
+    snake1.Enqueue(snake1.Dequeue());
+}
