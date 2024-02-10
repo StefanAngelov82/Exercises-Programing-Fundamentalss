eaafdaf91a3ac8b107b26880e8a1e9f19bca7a46
Stefan Angelov <ashmodean_@abv.bg>
2024-02-10 12:51:19 +0200

    upload task 30

---
+++ 44.Staks and Queues/30.CustomDoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomDoublyLinkedList
+{
+    public class DoublyLinkedList
+    {
+        public ListNode Head { get; set; }
+        public ListNode Tail { get; set; }
+
+        public int Count { get; set; }
+
+        public void AddLast(int value)
+        {
+            ListNode newNode = new ListNode(value);
+
+            Count++;
+
+            if (Head == null)
+            {
+                Head = newNode;
+                Tail = newNode;
+                return;
+            }
+
+            newNode.Previous = Tail;
+            Tail.Next = newNode;
+            Tail = newNode;
+        }
+        public void AddFirst(int value)
+        {
+            ListNode newNode = new ListNode(value);
+
+            Count++;
+
+            if (Head == null)
+            {
+                Head = newNode;
+                Tail = newNode;
+                return;
+            }
+
+            newNode.Next = Head;
+            Head.Previous = newNode;
+            Head = newNode;
+        }
+
+        public void RemoveFirst()
+        {
+            ListNode newHead = Head.Next;
+
+            Count--;
+
+            if (newHead == null)
+            {
+                Head = null;
+                Tail = null;
+                return;
+            }
+
+            newHead.Previous = null;
+            Head.Next = null;
+            Head = newHead;
+        }
+
+        public void RemoveLast()
+        {
+            ListNode newTail = Tail.Previous;
+
+            Count--;
+
+            if (newTail == null)
+            {
+                Head = null;
+                Tail = null;
+                return;
+            }
+
+            newTail.Next = null;
+            Tail.Previous = null;
+            Tail = newTail;
+        }
+
+        public void ForEach(Action<int> callBack)
+        {
+            ListNode node = Head;
+
+            while (node != null)
+            {
+                callBack(node.Value);
+                node = node.Next;
+            }
+        }
+
+        public int[] ToArray()
+        {
+            int[] array = new int[Count];
+            int index = 0;
+
+            ListNode node = Head;
+
+            while(node != null)
+            {
+                array[index++] = node.Value;
+                node = node.Next;
+            }
+
+            return array;
+        }
+    }
+}
+
