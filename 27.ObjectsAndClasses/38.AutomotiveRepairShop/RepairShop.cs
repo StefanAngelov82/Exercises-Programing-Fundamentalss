e2480537e63645904e87aee0aa2e638e395c95d8
Stefan Angelov <ashmodean_@abv.bg>
2024-02-15 13:01:28 +0200

    upload task 27

---
+++ 27.ObjectsAndClasses/38.AutomotiveRepairShop/RepairShop.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+
+namespace AutomotiveRepairShop
+{
+    public class RepairShop
+    {
+        public RepairShop(int capacity)
+        {
+            Capacity = capacity;
+            Vehicles = new List<Vehicle>();
+        }
+
+        public int Capacity { get; set; }
+        public List<Vehicle> Vehicles { get; set; }
+
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            if (Vehicles.Count < Capacity)            
+                Vehicles.Add(vehicle);           
+        }
+
+        public bool RemoveVehicle(string vin)
+        {
+            Vehicle? target = Vehicles.FirstOrDefault(x => x.VIN == vin);
+
+            if (target == null)
+                return false;
+            
+            Vehicles.Remove(target);
+            return true;
+        }
+
+        public int GetCount()
+        {
+            return Vehicles.Count;
+        }
+
+        public Vehicle GetLowestMileage()
+        {
+            return Vehicles.OrderBy(x => x.Mileage).First();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("Vehicles in the preparatory:");
+
+            foreach (var car in Vehicles)
+            {
+                sb.AppendLine(car.ToString());
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
