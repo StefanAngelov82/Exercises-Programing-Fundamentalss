68910c07d272bfaa252553bd54feb5bc03f9eb36
Stefan Angelov <ashmodean_@abv.bg>
2024-02-16 19:10:47 +0200

    hhh

---
+++ 24.Associative arrays/56.Songs Queue2/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Songs_Queue2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> songs = new(
+                Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries));
+
+            //bool IsDone = true;
+
+            while (songs.Any())
+            {
+                string[] inputArg = Console.ReadLine().Split();
+
+                string song = string.Join(" ", inputArg[1..]);
+
+                switch (inputArg[0])
+                {
+                    case "Play":
+
+                        //if (songs.Any())
+                            songs.Dequeue();
+
+                       // else                        
+                          // IsDone = false;                                           
+
+                        break;
+                       
+                    case "Add":
+
+                        if(songs.Contains(song))
+                            Console.WriteLine($"{song} is already contained!");
+
+                        else
+                            songs.Enqueue(song);
+
+                        break;
+
+                    case "Show":
+                        Console.WriteLine(string.Join(", ", songs));
+                        break;
+                }
+            }
+
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
---
+++ 27.ObjectsAndClasses/CarManufacturer/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+            car.FuelConsumption = 2;
+            car.FuelQuantity = 100;
+
+            car.Drive(50);
+            Console.WriteLine(car.WhoAmI());
+
+            
+        }
+    }
+}
