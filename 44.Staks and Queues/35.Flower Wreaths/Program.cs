b5c57dbdeb3f73296879ab5cd59e8c7348162f53
Stefan Angelov <ashmodean_@abv.bg>
2024-02-16 13:13:56 +0200

    upload task 35

---
+++ 44.Staks and Queues/35.Flower Wreaths/Program.cs
@@ -0,0 +1,62 @@
+﻿Queue<int> lilies = new Queue<int> (Console.ReadLine()
+                                        .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                        .Select(int.Parse));
+
+Stack<int> roses = new Stack<int>(Console.ReadLine()
+                                        .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                        .Select(int.Parse));
+
+int storage = 0;
+int wreaths = 0;
+
+while (lilies.Any() && roses.Any())
+{
+    if (lilies.Peek() + roses.Peek() == 15)
+    {
+        lilies.Dequeue();
+        roses.Pop();
+        wreaths++;
+    }
+    else
+    {
+        if (lilies.Peek() + roses.Peek() < 15)
+        {
+            storage += lilies.Dequeue() + roses.Pop();
+            continue;
+        }
+
+        int flowers = lilies.Dequeue() + roses.Pop();
+
+        while (flowers > 15)
+        {
+            flowers -= 2;
+
+            if (flowers == 15)
+            {
+                wreaths++;
+                break;
+            }
+
+            if (flowers < 15)
+            {
+                storage = +flowers;
+                break;
+            }
+        }       
+    }    
+}
+
+if (lilies.Any())
+{
+    storage += lilies.Sum();
+}
+
+if (roses.Any())
+{
+    storage += roses.Sum();
+}
+
+wreaths += storage / 15;
+
+if (wreaths >= 5) Console.WriteLine($"You made it, you are going to the competition with {wreaths} wreaths!");
+else Console.WriteLine($"You didn't make it, you need {5 - wreaths} wreaths more!");
