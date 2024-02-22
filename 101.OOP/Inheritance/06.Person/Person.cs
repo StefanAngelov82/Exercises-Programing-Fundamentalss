8d6e161ff794e59fad6906ce787c6416271360d8
Stefan Angelov <ashmodean_@abv.bg>
2024-02-22 20:28:00 +0200

    upload task 6

---
+++ 101.OOP/Inheritance/06.Person/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Person
+{
+    public class Person
+    {       
+        private int age;
+
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public virtual string Name { get; set; }
+        public virtual int Age 
+        {
+            get { return age; }
+            set 
+            { 
+                if (value > 0) 
+                    age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {age}";
+        }
+
+
+    }
+}
