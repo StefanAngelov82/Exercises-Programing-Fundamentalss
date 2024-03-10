e17aa30b1b85cce568d7060b0c483f2f51a2e1ea
Stefan Angelov <ashmodean_@abv.bg>
2024-03-10 13:23:59 +0200

    upload task 3

---
+++ 101.OOP/Polymorph/03.Shapes/Rectangle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+		private int height;
+        private int width;
+
+        public Rectangle(int height, int width)
+        {
+            Height = height;
+            Width = width;
+        }
+
+        public int Height
+        {
+			get { return height; }
+			private set { height = value; }
+		}		
+
+		public int Width
+        {
+			get { return width; }
+			private set { width = value; }
+        }
+
+        public override double CalculateArea()
+        {
+            return Height * Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Height + 2 * Width;
+        }
+    }
+}
---
+++ 101.OOP/Polymorph/03.Shapes/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = new Circle(5 );            
+
+            Console.WriteLine(shape.CalculateArea());
+            Console.WriteLine(shape.CalculatePerimeter());
+            Console.WriteLine(shape.Draw());
+        }
+    }
+}
