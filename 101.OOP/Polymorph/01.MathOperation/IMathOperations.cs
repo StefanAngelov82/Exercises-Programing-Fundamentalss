d3dbeaa67335d77c4fd237ecdea4eee96b042165
Stefan Angelov <ashmodean_@abv.bg>
2024-03-10 12:05:33 +0200

    upload task 10

---
+++ 101.OOP/Polymorph/01.MathOperation/IMathOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operations
+{
+    public interface IMathOperations
+    {
+
+        int Add(int a, int b);
+
+
+        double Add(double a, double b, double c);
+
+        decimal Add(decimal a, decimal b, decimal c);
+        
+
+    }
+}
