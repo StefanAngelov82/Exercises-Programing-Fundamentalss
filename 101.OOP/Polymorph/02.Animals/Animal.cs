d5575842e14d6b5d433024f8e88126e8848edbf0
Stefan Angelov <ashmodean_@abv.bg>
2024-03-10 12:43:26 +0200

    upload task 2

---
+++ 101.OOP/Polymorph/02.Animals/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        protected Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            this.favouriteFood = favouriteFood;
+        }
+
+        public string Name { get; protected set; }
+        public string favouriteFood { get; protected set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my favourite food is {this.favouriteFood}";
+        }
+       
+    }
+}
