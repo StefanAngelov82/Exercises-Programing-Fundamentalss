4663fb02feaa998cbde59b85f4b2baa439830a6d
Stefan Angelov <ashmodean_@abv.bg>
2024-03-16 18:35:28 +0200

    upload task 1

---
+++ 102 Exceptions/01.Square Root/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Principal;
+
+namespace Square_Root
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+			try
+			{
+                int N = int.Parse(Console.ReadLine());
+
+                if (N < 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(N), "Invalid number.");
+                }
+
+                Console.WriteLine(Math.Sqrt(N));
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Data must be digit");
+                Console.WriteLine(ex.Message);
+            }
+			catch (ArgumentOutOfRangeException ex)
+			{
+
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
