cf7cc8ce9b615735587de2252e5bb0b64db26c62
Stefan Angelov <ashmodean_@abv.bg>
2024-03-17 11:48:01 +0200

    upload task 02

---
+++ 102 Exceptions/02.Enter Numbers/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Enter_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int[] numbers = new int[10];
+            int start = 1;
+            int end = 100;
+
+
+            while (counter != 10)
+            {
+                try
+                {
+                    int number = int.Parse(Console.ReadLine());
+
+                    if ((number <= start || number >= end))
+                    {
+                        throw new ArgumentOutOfRangeException(null, $"Your number is not in range ({start} - {end})!");
+                    }
+
+                    start = number;
+                    numbers[counter] = number;
+                    counter++;
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Invalid Number!");
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+
+            Console.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
