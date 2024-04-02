30b1510450647e55f1c63978889d404485b6e458
Stefan Angelov <ashmodean_@abv.bg>
2024-04-02 23:46:25 +0300

    upload task 8

---
+++ 101.OOP/Polymorph/08.Highway To Peak/Core/Controller.cs
@@ -0,0 +1,95 @@
+﻿using HighwayToPeak.Core.Contracts;
+using HighwayToPeak.Models;
+using HighwayToPeak.Models.Contracts;
+using HighwayToPeak.Repositories;
+using HighwayToPeak.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighwayToPeak.Core
+{
+    public class Controller : IController
+    {
+        private PeakRepository peaks;
+        private ClimberRepository climbers;
+        private BaseCamp baseCamp;
+
+        public Controller()
+        {
+            this.peaks = new PeakRepository();
+            this.climbers = new ClimberRepository();
+            this.baseCamp = new BaseCamp();
+        }
+
+        public string AddPeak(string name, int elevation, string difficultyLevel)
+        {
+            if (peaks.Get(name) != null)            
+                return String.Format(OutputMessages.PeakAlreadyAdded, name);
+
+
+            if (difficultyLevel != "Extreme" && difficultyLevel != "Hard" && difficultyLevel != "Moderate")
+                return String.Format(OutputMessages.PeakDiffucultyLevelInvalid, difficultyLevel);
+            
+            peaks.Add(new Peak(name, elevation, difficultyLevel));
+
+            return String.Format(OutputMessages.PeakIsAllowed, name , nameof(PeakRepository));
+        }
+
+        public string AttackPeak(string climberName, string peakName)
+        {
+            IClimber climber = climbers.Get(climberName);
+            IPeak peak = peaks.Get(peakName);
+
+            if (climber == null)
+                return String.Format(OutputMessages.ClimberNotArrivedYet, climberName);
+
+            if (peak == null)
+                return String.Format(OutputMessages.PeakIsNotAllowed, peakName);
+
+            if (!baseCamp.Residents.Contains(climberName))
+                return String.Format(OutputMessages.ClimberNotFoundForInstructions, climberName, peakName);
+
+            if (peak.DifficultyLevel == "Extreme" && climber.GetType().Name == nameof(NaturalClimber))
+                return String.Format(OutputMessages.NotCorrespondingDifficultyLevel, climberName, peakName);
+
+            return "";
+        }
+
+        public string BaseCampReport()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CampRecovery(string climberName, int daysToRecover)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string NewClimberAtCamp(string name, bool isOxygenUsed)
+        {
+            if (climbers.Get(name) != null)
+                return String.Format(OutputMessages.ClimberCannotBeDuplicated, name, nameof(ClimberRepository));
+
+            IClimber climber;
+
+            if (isOxygenUsed)
+                climber = new OxygenClimber(name);
+
+            else
+                climber = new NaturalClimber(name);
+
+            baseCamp.ArriveAtCamp(name);
+            climbers.Add(climber);
+
+            return String.Format(OutputMessages.ClimberArrivedAtBaseCamp, name);
+        }
+
+        public string OverallStatistics()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 101.OOP/Polymorph/08.Highway To Peak/Models/Climber.cs
@@ -0,0 +1,83 @@
+﻿using HighwayToPeak.Models.Contracts;
+using HighwayToPeak.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighwayToPeak.Models
+{
+    public abstract class Climber : IClimber
+    {
+        private string name;
+        private int stamina;
+        private HashSet<string> conqueredPeaks;
+
+        protected Climber(string name, int stamina)
+        {
+            Name = name;
+            Stamina = stamina;
+            conqueredPeaks = new HashSet<string>();
+        }
+
+        public string Name
+        {
+            get { return name; }
+            private set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentNullException(ExceptionMessages.ClimberNameNullOrWhiteSpace);
+
+                name = value; 
+            }
+        }
+        public int Stamina
+        {
+            get { return stamina; }
+            protected set 
+            {
+                if (value > 10)                
+                    value = 10;
+
+                if (value < 0)
+                    value = 0;
+
+                stamina = value; 
+            }
+        }
+        public IReadOnlyCollection<string> ConqueredPeaks => conqueredPeaks;
+
+        public void Climb(IPeak peak)
+        {            
+                conqueredPeaks.Add(peak.Name);
+
+            if (peak.DifficultyLevel == "Extreme")
+                Stamina -= 6;
+
+            else if (peak.DifficultyLevel == "Hard")
+                Stamina -= 4;
+
+            else 
+                Stamina -= 2;
+        }
+
+        public abstract void Rest(int daysCount);
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{GetType().Name} - Name: {Name}, Stamina: {Stamina}");
+
+            if (conqueredPeaks.Count == 0)
+                sb.AppendLine("Peaks conquered: no peaks conquered");
+
+            else
+                sb.AppendLine($"Peaks conquered: {conqueredPeaks.Count}");
+
+
+            return sb.ToString().Trim();
+        }
+    }
+}
