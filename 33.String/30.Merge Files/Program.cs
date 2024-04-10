4fb9cea573660323ec4e93bb99c7456a11c47e18
Stefan Angelov <ashmodean_@abv.bg>
2024-04-11 00:10:12 +0300

    upload task 30

---
+++ 33.String/30.Merge Files/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Merge_Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text1 = File.ReadAllLines(@"../../../text1.txt");
+            string[] text2 = File.ReadAllLines(@"../../../text2.txt");
+
+            using (StreamWriter SW = new StreamWriter(@"../../../result.txt"))
+            {
+                for (int i = 0; i < Math.Min(text1.Length, text2.Length); i++)
+                {
+                    SW.WriteLine(text1[i]);
+                    SW.WriteLine(text2[i]);
+                }
+
+                if (text1.Length > text2.Length)
+                {
+                    int start = text1.Length - (text1.Length - text2.Length);
+
+                    for (int i = start; i < text1.Length; i++)
+                    {
+                        SW.WriteLine(text1[i]);
+                    }
+                }
+            }
+        }
+    }
+}
