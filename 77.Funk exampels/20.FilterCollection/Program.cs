d200f5752a8d2f621159d3a12c113dcb13f87dac
Stefan Angelov <ashmodean_@abv.bg>
2024-04-13 17:44:59 +0300

    upload task 20

---
+++ 77.Funk exampels/20.FilterCollection/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FilterCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> num = new List<int>() {1, 5, 17, 11, 2 ,6 , 15, };
+
+            Console.WriteLine(string.Join(" ", Filter(num, x => x > 7)));
+        }
+
+        public static List<int> Filter(
+                List<int> numbers,
+                Func<int, bool> filter)
+        {
+            var result = new List<int>();
+
+            foreach (var number in numbers)
+            {
+                if (filter(number))
+                {
+                    result.Add(number);
+                }
+            }
+
+            return result;
+        }
+    }
+}
