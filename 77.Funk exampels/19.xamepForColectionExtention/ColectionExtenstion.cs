950159a5bc0eb90606114392f737b62240c35414
Stefan Angelov <ashmodean_@abv.bg>
2024-04-13 17:31:27 +0300

    upload task 19

---
+++ 77.Funk exampels/19.xamepForColectionExtention/ColectionExtenstion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamepForColectionExtention
+{
+    public static class CollectionExtension
+    {
+
+        public static string TrimDashes(this string input)
+            =>  input.Trim('-');
+
+        public static IEnumerable<T> ForEach1<T>(this IEnumerable<T>hashSet, Action<T> action)
+        {
+            foreach (var item in hashSet)
+            {
+                action(item);
+            }
+
+            return hashSet;
+        }
+       
+    }
+}
---
+++ 77.Funk exampels/19.xamepForColectionExtention/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ExamepForColectionExtention
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /// remove '-'
+            String text = "--Text---";
+
+            Console.WriteLine(text.TrimDashes());
+
+            /// forEach HashSet
+
+            HashSet<int> set = new HashSet<int>() { 1, 3, 4, 7};
+            HashSet<string> set1 = new HashSet<string>() { "1", "2", "4"};
+
+            set.ForEach1(x => Console.WriteLine((x + 1)));
+            set1.ForEach1(x => Console.WriteLine((x + "&")));
+
+
+            Console.WriteLine(string.Join("/", set));
+        }
+    }
+}
