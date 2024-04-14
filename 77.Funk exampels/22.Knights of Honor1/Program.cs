7f4aa1a2d15b9033b540e7684e460bcf279146b7
Stefan Angelov <ashmodean_@abv.bg>
2024-04-14 10:35:41 +0300

    upload task 22

---
+++ 77.Funk exampels/22.Knights of Honor1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Knights_of_Honor1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Action<string> filter = AddiningPrefix();
+            Array.ForEach(names, filter);
+        }
+
+        private static Action<string> AddiningPrefix()
+        {
+            return x => Console.WriteLine($"Sir {x}");
+        }
+    }
+}
