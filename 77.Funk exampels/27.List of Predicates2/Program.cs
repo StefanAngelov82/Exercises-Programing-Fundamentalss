774b422076223ceb5686f9583fe8ed8aa47d2809
Stefan Angelov <ashmodean_@abv.bg>
2024-04-14 23:12:34 +0300

    upload task 27

---
+++ 77.Funk exampels/27.List of Predicates2/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace List_of_Predicates2
+{
+    internal class Program
+    {
+        public delegate bool Filter(int number);
+
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] divisors = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Filter IsDivided = Divided(divisors);
+
+            int[] numbers = new int[N];
+
+            for (int i = 0; i < numbers.Length; i++)            
+                numbers[i] = i + 1;
+
+            Console.WriteLine(string.Join(" ", numbers.Where(x => IsDivided(x))));
+
+        }
+
+        private static Filter Divided(int[] divisors)
+        {
+            Predicate<int> predicate =
+                (x) =>
+                {
+                    for (int i = 0; i < divisors.Length; i++)
+                    {
+                        if (x % divisors[i] != 0)                        
+                            return false;                        
+                    }
+
+                    return true;
+                };
+
+            return new Filter(predicate);
+        }
+    }
+}
