b721992cd007ce08023d8c44bed21eb8b3532e9a
Stefan Angelov <ashmodean_@abv.bg>
2024-04-14 11:06:21 +0300

    upload task 23

---
+++ 77.Funk exampels/23.Custom Min Function1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Custom_Min_Function1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Func<int[], int> minNumber = MINValue();            
+
+            Console.WriteLine(minNumber(numbers));
+        }
+
+        public static Func<int[], int> MINValue()
+        {
+            Func<int[], int> min =
+                 (x) =>
+                 {
+                     int MinValue = int.MaxValue;
+
+                     foreach (int number in x)
+                     {
+                         if (MinValue > number)                       
+                             MinValue = number;                         
+                     }
+
+                     return MinValue;
+                 };
+
+            return min;
+        }
+    }      
+}
