674ef5a9cccaa960715169cb1805843ef7a55f31
Stefan Angelov <ashmodean_@abv.bg>
2024-04-14 21:22:57 +0300

    upload task 26

---
+++ 77.Funk exampels/26.Predicate for names1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Predicate_for_names1
+{
+    internal class Program
+    {
+
+        public delegate bool Filter(string name);
+
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine().Split();
+
+            Filter filter = Filtretion(N);
+
+            names.Where(x => filter(x)).ToList().ForEach(x => Console.WriteLine(x));
+        }
+
+        static Filter Filtretion(int N)
+        {
+           Predicate<string> filter =
+                (x) => x.Length <= N;
+
+            return new Filter(filter);
+        }
+    }
+}
