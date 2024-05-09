5461f71631e062ab8331966489b5ea8b55da1267
Stefan Angelov <ashmodean_@abv.bg>
2024-05-09 21:21:09 +0300

    upload task1

---
+++ 101.OOP/Reflection/01.HarvestfieldsDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using HarvestFieldsDemo;
+using System.Reflection;
+
+namespace HarvestfieldsDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FieldInfo[] fieldInfos = typeof(HarvestingFields).GetFields((BindingFlags)60);
+
+            string? input = String.Empty;
+
+            while ((input = Console.ReadLine()) != "HARVEST")    
+            {
+                IEnumerable<FieldInfo>? filteredFields = null; 
+
+                switch (input)
+                {
+                    case "private":
+                        filteredFields = FilterFields(input, fieldInfos);
+                        break;
+
+                    case "protected":
+                        filteredFields = FilterFields(input, fieldInfos);
+                        break;
+                    case "public":
+                        filteredFields = FilterFields(input, fieldInfos);
+                        break;
+                    case "all":
+                        filteredFields = fieldInfos;
+                        break;
+                }
+
+                foreach (FieldInfo field in filteredFields!)
+                {
+                    string modifier = (field.Attributes.ToString() == "Family") ? "protected" : field.Attributes.ToString();
+
+                    Console.WriteLine($"{modifier} {field.GetModifiedFieldType().Name} {field.Name}");
+                    //accessMod       field type                            field name
+                }
+            }
+        }
+
+        private static IEnumerable<FieldInfo>? FilterFields(string filter, FieldInfo[] fieldInfos)
+        {
+            if  (filter == "private")
+                return fieldInfos.Where(f => f.IsPrivate);
+            
+            else if (filter == "protected")
+                return fieldInfos.Where(f => f.IsFamily);
+
+            else
+                return fieldInfos.Where(f => f.IsPublic);
+        }                   
+        
+    }
+}
