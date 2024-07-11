0f15c596fa6455fc67b6ccb86e3818f39a3cc226
Stefan Angelov <ashmodean_@abv.bg>
2024-07-11 21:05:39 +0300

    upload task

--- 200.Db/03.ADVANCEQEX/BookShop/StartUp.cs
+++ 200.Db/03.ADVANCEQEX/BookShop/StartUp.cs
@@ -3,6 +3,11 @@
     using BookShop.Models.Enums;
     using Data;
     using Initializer;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Data;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq.Expressions;
     using System.Text;
     using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
@@ -13,7 +18,7 @@
             using var db = new BookShopContext();
             //DbInitializer.ResetDatabase(db);
 
-            Console.WriteLine(GetGoldenBooks(db));
+            Console.WriteLine(GetBooksByCategory(db, "horror mystery drama"));
         }
 
         public static string GetBooksByAgeRestriction(BookShopContext context, string command)
@@ -28,8 +33,8 @@
 
             else ageRestriction = AgeRestriction.Adult;
 
-           // if (!Enum.TryParse(command, true, out ageRestriction))
-               // return string.Empty;
+            // if (!Enum.TryParse(command, true, out ageRestriction))
+            // return string.Empty;
 
             var books = context.Books
                 .Where(b => b.AgeRestriction == ageRestriction)
@@ -38,7 +43,7 @@
 
                     b.Title
                 })
-                .OrderBy(b =>b.Title)
+                .OrderBy(b => b.Title)
                 .ToList();
 
             StringBuilder sb = new StringBuilder();
@@ -52,7 +57,7 @@
         public static string GetGoldenBooks(BookShopContext context)
         {
             var books = context.Books
-                .Where(b =>b.EditionType == EditionType.Gold &&
+                .Where(b => b.EditionType == EditionType.Gold &&
                        b.Copies < 5_000)
                 .Select(b => new
                 {
@@ -68,6 +73,73 @@
                 sb.AppendLine(book.BookTitle);
 
             return sb.ToString().Trim();
+        }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            var books = context.Books
+                .Where(b => b.Price > 40)
+                .Select(b => new
+                {
+                    b.Title,
+                    b.Price
+                })
+                .OrderByDescending(b => b.Price)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var book in books)
+                sb.AppendLine($"{book.Title} - ${book.Price:F2}");
+
+            return sb.ToString().Trim();
+        }
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+
+            var books = context.Books
+                .Where(b => b.ReleaseDate.HasValue &&
+                       b.ReleaseDate.Value.Year != year)
+                .Select(b => new
+                {
+                    b.BookId,
+                    b.Title
+                })
+                .OrderBy(b => b.BookId)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var book in books)
+                sb.AppendLine(book.Title);
+
+            return sb.ToString().Trim();
+        }
+
+        static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            string[] givenCategory = input
+                .ToLower()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            var books = context.BooksCategories
+                .Where(Check(givenCategory))
+                .Select(bc => bc.Book.Title)
+                .OrderBy(t => t)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var book in books)
+                sb.AppendLine($"{book}");
+
+            return sb.ToString().Trim();
+
+            Expression<Func<Models.BookCategory, bool>> Check(string[] givenCategory)
+            {
+                return bc => givenCategory.Contains(bc.Category.Name);
+            }
         }
     }
 }
